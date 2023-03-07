16513c3b4095f02ee6002c3f0ddcdfe3117c1ba9
黎サブ 星野 <taifu9920923@gmail.com>
2023-03-07 23:24:20 +0800

    Update

--- app/Http/Controllers/apiController.php
+++ app/Http/Controllers/apiController.php
@@ -24,7 +24,7 @@ class apiController extends Controller
         $chat_id = $request->input('chat_id');
         $output = $request->input('output');
         error_log($output);
-        if ($output && $output.trim() == "") $output = "[Sorry, This LLM generate nothing as feedback.]";
+        if (!$output && $output.trim() == "") $output = "[Sorry, This LLM generate nothing as feedback.]";
         error_log($output);
         if ($input && $input.trim() == "") return response()->json(['result' => false]);
         try{
