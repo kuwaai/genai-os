1c0461bfc13f2540f822a9a9f7d1dc10147b4162
Drift <taifu9920923@gmail.com>
2023-03-07 17:13:57 +0000

    Update

--- executables/startWeb_Public.sh
+++ executables/startWeb_Public.sh
@@ -1,2 +1,8 @@
 cd ..
-npm run build & php artisan serve --host 0.0.0.0
+composer dump-autoload --optimize
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan optimize
+npm run build
+sudo php artisan serve --host 0.0.0.0 --port=80
