4bba782d59b4d07e3d2ce3e434407a9130ea818d
黎サブ 星野 <taifu9920923@gmail.com>
2023-03-26 11:49:49 +0800

    Update

--- executables/sh/install.sh
+++ executables/sh/install.sh
@@ -1,5 +1,5 @@
 cd ../..
-sudo apt install php-pgsql php-xml php php-curl php-redis
+sudo apt install php-pgsql php-xml php php-curl php-redis composer redis
 composer update
 php artisan key:generate
 php artisan migrate
