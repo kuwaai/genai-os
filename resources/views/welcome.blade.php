6b09b99e17606912eda2f486315f16cd54c80486
黎サブ 星野 <taifu9920923@gmail.com>
2023-04-03 03:15:10 +0800

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,7 +48,7 @@
 
         <div class="max-w-7xl mx-auto p-6 lg:p-8">
             <div class="flex items-center flex-col">
-                <h3 class="text-5xl text-white font-bold mb-2 text-cyan-200">TAIDE Chat</h3>
+                <h3 class="text-5xl font-bold mb-2 text-cyan-200">TAIDE Chat</h3>
                 <p class="text-lg text-white">(<span class="text-yellow-200">T</span>rustworthy <span class="text-yellow-200">AI</span> <span class="text-yellow-200">D</span>ialog <span class="text-yellow-200">E</span>ngine Chat)</p>
                 <p class="text-lg text-cyan-200">Free to chat with Open Source LLMs!</p>
             </div>
