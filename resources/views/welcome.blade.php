1074bb8a9ebeac3736b8b0495ddb191f525eef46
黎サブ 星野 <taifu9920923@gmail.com>
2023-04-19 10:19:38 +0800

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="antialiased">
+<body class="antialiased scrollbar">
     <div
         class="relative z-9999 sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
         @if (Route::has('login'))
