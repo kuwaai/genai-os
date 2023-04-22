0eafbaabcbbf03a92a81f755ac8468fcd9c512e7
黎サブ 星野 <taifu9920923@gmail.com>
2023-04-22 15:12:34 +0800

    update

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -116,6 +116,7 @@ class ChatController extends Controller
                                 # each token should restore 5 seconds of timeout, maximum is 300 seconds(5 mins)
                                 if ($timeouts < 300) {
                                     $timeouts = time() - $start_time + 5;
+                                    export($timeouts);
                                     set_time_limit($timeouts);
                                 }
                                 #Flush the buffer
