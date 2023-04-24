c0976f3fe3533e765d87c778002fb0a138f6b86e
黎サブ 星野 <taifu9920923@gmail.com>
2023-04-24 23:57:41 +0800

    Update

--- app/Jobs/RequestChat.php
+++ app/Jobs/RequestChat.php
@@ -18,7 +18,7 @@ class RequestChat implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
     private $input, $API, $msgtime, $history_id, $user_id, $chat_id;
-    public $tries = 30;  # 10 seconds pending for each, 300 seconds in total
+    public $tries = 30; # 10 seconds pending for each, 300 seconds in total
     /**
      * Create a new job instance.
      */
@@ -40,62 +40,65 @@ class RequestChat implements ShouldQueue
         try {
             Redis::set('msg' . $this->history_id, '');
             $client = new Client();
-            $response = $client->post("http://localhost:9000/status", [
+            $response = $client->post('http://localhost:9000/status', [
                 'headers' => ['Content-Type' => 'application/x-www-form-urlencoded'],
                 'form_params' => [
-                    'name' => "Test",
-                    "userid" => $this->user_id
+                    'name' => 'Test',
+                    'userid' => $this->user_id,
                 ],
                 'stream' => true,
             ]);
-            if ($response->getBody()->getContents() == "BUSY") {
+            if ($response->getBody()->getContents() == 'BUSY') {
                 $this->release(10);
-                return;
-            }
-            $response = $client->post("http://localhost:9000/", [
-                'headers' => ['Content-Type' => 'application/x-www-form-urlencoded'],
-                'form_params' => [
-                    'input' => $this->input,
-                    'name' => "Test",
-                    "userid" => $this->user_id
-                ],
-                'stream' => true,
-            ]);
-            
-            $stream = $response->getBody();
-            $buffer = '';
-            while (!$stream->eof()) {
-                $chunk = $stream->read(1);
-                $buffer .= $chunk;
-                $bufferLength = mb_strlen($buffer, 'UTF-8');
-                $messageLength = null;
-                for ($i = 1; $i <= $bufferLength; $i++) {
-                    if (ord($buffer[$i - 1]) < 128 || $i == $bufferLength) {
-                        $messageLength = $i;
-                        break;
+            } else {
+                try {
+                    $response = $client->post('http://localhost:9000/', [
+                        'headers' => ['Content-Type' => 'application/x-www-form-urlencoded'],
+                        'form_params' => [
+                            'input' => $this->input,
+                            'name' => 'Test',
+                            'userid' => $this->user_id,
+                        ],
+                        'stream' => true,
+                    ]);
+
+                    $stream = $response->getBody();
+                    $buffer = '';
+                    while (!$stream->eof()) {
+                        $chunk = $stream->read(1);
+                        $buffer .= $chunk;
+                        $bufferLength = mb_strlen($buffer, 'UTF-8');
+                        $messageLength = null;
+                        for ($i = 1; $i <= $bufferLength; $i++) {
+                            if (ord($buffer[$i - 1]) < 128 || $i == $bufferLength) {
+                                $messageLength = $i;
+                                break;
+                            }
+                        }
+                        if ($messageLength !== null) {
+                            $message = mb_substr($buffer, 0, $messageLength, 'UTF-8');
+                            if (mb_check_encoding($message, 'UTF-8')) {
+                                Redis::set('msg' . $this->history_id, Redis::get('msg' . $this->history_id) . $message);
+                                $buffer = mb_substr($buffer, $messageLength, null, 'UTF-8');
+                            }
+                        }
+                        if (strlen(Redis::get('msg' . $this->history_id)) > 700) {
+                            break;
+                        }
                     }
-                }
-                if ($messageLength !== null) {
-                    $message = mb_substr($buffer, 0, $messageLength, 'UTF-8');
-                    if (mb_check_encoding($message, 'UTF-8')) {
-                        Redis::set('msg' . $this->history_id, Redis::get('msg' . $this->history_id) . $message);
-                        $buffer = mb_substr($buffer, $messageLength, null, 'UTF-8');
+                    if (Redis::get('msg' . $this->history_id) == '') {
+                        Redis::set('msg' . $this->history_id, '[Oops, seems like LLM given empty message as output!]');
                     }
+                } catch (Exception $e) {
+                    Redis::set('msg' . $this->history_id, Redis::get('msg' . $this->history_id) . "\n[Sorry, something is broken!]");
+                } finally {
+                    $history = new Histories();
+                    $history->fill(['msg' => trim(Redis::get('msg' . $this->history_id)), 'chat_id' => $this->chat_id, 'isbot' => true, 'created_at' => $this->msgtime]);
+                    $history->save();
+                    Redis::lrem('usertask_' . $this->user_id, 0, $this->history_id);
                 }
-                if (strlen(Redis::get('msg' . $this->history_id)) > 700) {
-                    break;
-                }
-            }
-            if (Redis::get('msg' . $this->history_id) == '') {
-                Redis::set('msg' . $this->history_id, '[Oops, seems like LLM given empty message as output!]');
             }
         } catch (Exception $e) {
-            Redis::set('msg' . $this->history_id, Redis::get('msg' . $this->history_id) . "\n[Sorry, something is broken!]");
-        } finally {
-            $history = new Histories();
-            $history->fill(['msg' => trim(Redis::get('msg' . $this->history_id)), 'chat_id' => $this->chat_id, 'isbot' => true, 'created_at' => $this->msgtime]);
-            $history->save();
-            Redis::lrem('usertask_' . $this->user_id, 0, $this->history_id);
         }
     }
 }
