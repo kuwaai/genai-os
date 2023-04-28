b1472c607378755652ea187f8e2147ba9d9db7c5
黎サブ 星野 <taifu9920923@gmail.com>
2023-04-29 01:02:23 +0800

    Update

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -104,9 +104,9 @@ class ChatController extends Controller
                 $new_listening = Redis::lrange('usertask_' . Auth::user()->id, 0, -1);
                 foreach ($listening as $history_id) {
                     $finished = false;
+                    Log::Debug(array_search($history_id, $new_listening) . " | " . $history_id);
                     if (array_search($history_id, $new_listening)) {
                         $finished = true;
-                        Log::Debug(array_search($history_id, $new_listening) . " | " . $history_id);
                     }
                     $result = Redis::get('msg' . $history_id);
                     # Validate and convert for the encoding of incoming message
