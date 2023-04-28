bbd25dfe184a2d78118da77a73efd6978a716930
黎サブ 星野 <taifu9920923@gmail.com>
2023-04-28 21:16:37 +0800

    Update

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -134,6 +134,7 @@ class ChatController extends Controller
                         Redis::del('msg' . $history_id);
                         unset($lengths[$history_id]);
                         unset($listening[$history_id]);
+                        break;
                     }
                 }
                 usleep(100000);
