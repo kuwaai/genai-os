3213279e6aee52a886049d73bdf7426ec75f79e9
黎サブ 星野 <taifu9920923@gmail.com>
2023-05-01 17:56:18 +0800

    Update

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -131,8 +131,17 @@ class ChatController extends Controller
                         }
                         $newData = mb_substr($msg, $lengths[$history_id], null, 'utf-8');
                         $length = mb_strlen($newData, 'utf-8');
-                        echo 'data: ' . $history_id . ',' . $newData . "___\n\n";
-                        
+                        for ($i = 0; $i < $length; $i++) {
+                            # Make sure the data is correctly encoded and output a character at a time
+                            $char = mb_substr($newData, $i, 1, 'utf-8');
+                            if (mb_check_encoding($char, 'utf-8')) {
+                                $lengths[$history_id] += 1;
+                                echo 'data: ' . $history_id . ',' . $char . "___\n\n";
+                                # Flush the buffer
+                                ob_flush();
+                                flush();
+                            }
+                        }
                     }
                 });
             }
