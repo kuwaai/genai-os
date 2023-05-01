ac5a34ebd1f4e5caaaa7463305a5a14f497509a4
黎サブ 星野 <taifu9920923@gmail.com>
2023-05-01 23:59:24 +0800

    Update

--- app/Jobs/RequestChat.php
+++ app/Jobs/RequestChat.php
@@ -100,7 +100,6 @@ class RequestChat implements ShouldQueue
                     $history->fill(['msg' => trim($tmp), 'chat_id' => $this->chat_id, 'isbot' => true, 'created_at' => $this->msgtime]);
                     $history->save();
                     Redis::publish($this->history_id, "Ended Ended");
-                    Redis::publish($this->history_id, "Ended Ended");
                     Redis::lrem('usertask_' . $this->user_id, 0, $this->history_id);
                 }
             }
