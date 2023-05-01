b2d3158bd12b14422b804c890142d505c73f4e4a
黎サブ 星野 <taifu9920923@gmail.com>
2023-05-02 00:07:53 +0800

    Update

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -105,34 +105,29 @@ class ChatController extends Controller
         $response->setCallback(function () {
             $listening = Redis::lrange('usertask_' . Auth::user()->id, 0, -1);
             if (count($listening) > 0) {
-                try {
-                    Redis::subscribe($listening, function ($message, $raw_history_id) use ($listening) {
-                        [$type, $msg] = explode(' ', $message, 2);
-                        $history_id = substr($raw_history_id, strrpos($raw_history_id, '_') + 1);
-                        if ($type == 'Ended') {
-                            $key = array_search($history_id, $listening);
-                            if ($key !== false) {
-                                unset($listening[$key]);
-                            }
-                            if (count($listening) == 0) {
-                                echo "event: close\n\n";
-                                ob_flush();
-                                flush();
-                                Redis::unsubscribe();
-                                $response->close();
-                                die($message);
-                            }
-                        } elseif ($type == 'New') {
-                            echo 'data: ' . $history_id . ',' . $msg . "\n\n";
-                            # Flush the buffer
+                Redis::subscribe($listening, function ($message, $raw_history_id) use ($listening, $response) {
+                    [$type, $msg] = explode(' ', $message, 2);
+                    $history_id = substr($raw_history_id, strrpos($raw_history_id, '_') + 1);
+                    if ($type == 'Ended') {
+                        $key = array_search($history_id, $listening);
+                        if ($key !== false) {
+                            unset($listening[$key]);
+                        }
+                        if (count($listening) == 0) {
+                            echo "event: close\n\n";
                             ob_flush();
                             flush();
+                            Redis::unsubscribe();
+                            $response->close();
                         }
-                    });
-                } catch (\RedisException $exception) {
-                }
+                    } elseif ($type == 'New') {
+                        echo 'data: ' . $history_id . ',' . $msg . "\n\n";
+                        # Flush the buffer
+                        ob_flush();
+                        flush();
+                    }
+                });
             }
-            Log::Debug('Done');
         });
 
         return $response;
