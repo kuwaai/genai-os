a4baad80d1f9ef0885917afecc1a4310ff33d544
黎サブ 星野 <taifu9920923@gmail.com>
2023-05-02 16:08:07 +0800

    Update

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -99,38 +99,15 @@ class ChatController extends Controller
         $response = response()->stream(function () {
             $listening = Redis::lrange('usertask_' . Auth::user()->id, 0, -1);
             if (count($listening) > 0) {
-                try{
+                try {
                     $client = new Client([
                         'scheme' => 'tcp',
                         'host' => '127.0.0.1',
                         'port' => 6379,
                     ]);
-                    $client->subscribe($listening, function ($a) use ($listening, $client) {
-                        [$type, $msg] = explode(' ', $message, 2);
-                        $history_id = substr($raw_history_id, strrpos($raw_history_id, '_') + 1);
-                        Log::Debug($type);
-                        if ($type == 'Ended') {
-                            $key = array_search($history_id, $listening);
-                            if ($key !== false) {
-                                unset($listening[$key]);
-                            }
-                            if (count($listening) == 0) {
-                                echo "event: close\n\n";
-                                ob_flush();
-                                flush();
-                                $client->unsubscribe();
-                            }
-                        } elseif ($type == 'New') {
-                            echo 'data: ' . $history_id . ',' . $msg . "\n\n";
-                            # Flush the buffer
-                            ob_flush();
-                            flush();
-                        }
-                    });
-                }catch (Exception $e){
-
+                } catch (Exception $e) {
                 }
-                Log::Debug("Test");
+                Log::Debug('Test');
             }
         });
         $response->headers->set('Content-Type', 'text/event-stream');
