87dff3a90866e33b7152b63c04d7b5bb5b51263d
黎サブ 星野 <taifu9920923@gmail.com>
2023-05-13 01:15:59 +0800

    Update

--- executables/docker/startWeb_Public.sh
+++
@@ -1,8 +0,0 @@
-cd ../..
-composer dump-autoload --optimize
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-php artisan optimize
-npm run build
-php artisan serve --host 0.0.0.0 --port=80
