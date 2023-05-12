4a981c2c2a12466d62daf1a66c6ebcb531683ab1
黎サブ 星野 <taifu9920923@gmail.com>
2023-05-13 01:44:33 +0800

    Update

--- executables/docker/install.sh
+++ executables/docker/install.sh
@@ -1,7 +1,7 @@
 cd ../..
 composer update
-php artisan key:generate
-php artisan migrate
+php artisan key:generate --force
+php artisan migrate --force
 rm public/storage
 php artisan storage:link
 npm install
