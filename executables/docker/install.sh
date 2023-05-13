381df9f8f6c105c15de245e83a8dcaf0f4e8770e
黎サブ 星野 <taifu9920923@gmail.com>
2023-05-14 03:52:45 +0800

    Update

--- executables/docker/install.sh
+++ executables/docker/install.sh
@@ -6,3 +6,9 @@ php artisan db:seed --class=DefaultSeeder --force
 rm public/storage
 php artisan storage:link
 npm install
+composer dump-autoload --optimize
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan optimize
+npm run build
