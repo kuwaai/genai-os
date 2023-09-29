3691d7c75fff131e4bf97aad54f5647da3724c61
黎サブ 星野 <taifu9920923@gmail.com>
2023-09-29 18:36:41 +0800

    update

--- main.py
+++ main.py
@@ -7,15 +7,13 @@ from flask_sse import ServerSentEventsBlueprint
 from routes.worker import worker
 from routes.chat import chat
 
-
-if __name__ == '__main__':
-    app = Flask(__name__)
-    app.config["REDIS_URL"] = "redis://localhost:6379/0"
-    sse = ServerSentEventsBlueprint('sse', __name__)
-    app.register_blueprint(sse, url_prefix='/')
-    app.register_blueprint(worker, url_prefix=f'/{version}/worker')
-    app.register_blueprint(chat, url_prefix=f'/{version}/chat')
-    print("Route list:\n","\n".join([str(i) for i in app.url_map.iter_rules()]))
-    app = pywsgi.WSGIServer(("0.0.0.0",9000), app, spawn=10)
-    print("\n\nServer started")
-    app.serve_forever()
+app = Flask(__name__)
+app.config["REDIS_URL"] = "redis://localhost:6379/0"
+sse = ServerSentEventsBlueprint('sse', __name__)
+app.register_blueprint(sse, url_prefix='/')
+app.register_blueprint(worker, url_prefix=f'/{version}/worker')
+app.register_blueprint(chat, url_prefix=f'/{version}/chat')
+print("Route list:\n","\n".join([str(i) for i in app.url_map.iter_rules()]))
+app = pywsgi.WSGIServer(("0.0.0.0",9000), app, spawn=10)
+print("\n\nServer started")
+app.serve_forever()
