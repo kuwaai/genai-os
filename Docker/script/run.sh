ff6504810599c6b71ee30948688c35d188d7be2c
MonkeyHuang <iijusuu0815110336@gmail.com>
2023-09-30 15:45:13 +0800

    bug fixes in docker

--- Docker/script/run.sh
+++ Docker/script/run.sh
@@ -1,6 +1,6 @@
 cd /var/www/html
 # Install packages
-cp /.env /var/www/html/LLM_Project/
+# cp /.env /var/www/html/LLM_Project/
 cd /var/www/html/LLM_Project/executables/docker
 chmod +x install.sh
 ./install.sh
@@ -14,13 +14,15 @@ rm * -rf
 ln -s ../sites-available/llmproject .
 service nginx restart
 cp /var/www/html/LLM_Project/www.conf /etc/php/8.1/fpm/pool.d/www.conf
+cp /var/www/html/LLM_Project/php.ini /etc/php/8.1/fpm/php.ini
 service php8.1-fpm start
+
 # Start the agent program
-cd /var/www/html/LLM_Project/LLMs
+cd /agent
 pip install -r requirements.txt
 # Start a worker
 chmod +x /var/www/html/LLM_Project/executables/docker/work.sh
 screen -L -dmS worker1 bash -c "cd /var/www/html/LLM_Project/executables/docker/ && ./work.sh"
 # Start agent
-cd 0.0.3
+cd /agent
 python3 agent.py
