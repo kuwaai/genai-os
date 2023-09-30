6232e9d74a66ca638c0209cd67bde220ef924794
Yung-Hsiang Hu <iftnt1999@gmail.com>
2023-10-01 07:39:25 +0800

    [DocQA] new: Pre-cache model
    This patch add pre-cache instruction in the building step to reduce the
    initalize time at the runtime.

---
+++ model_workers/document_question_answering/pre_cache_models.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+from langchain.embeddings import HuggingFaceEmbeddings
+
+def main():
+    models = [
+        {
+            'class': HuggingFaceEmbeddings,
+            'name': 'paraphrase-multilingual-MiniLM-L12-v2' 
+        }
+    ]
+
+    for model in models:
+        print('Pre-caching model: {}'.format(model['name']))
+        model['class'](model_name=model['name'])
+
+if __name__ == '__main__':
+    main()
