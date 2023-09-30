361a4b918b595d59e5d22dfe8dae61a64accddca
黎サブ 星野 <taifu9920923@gmail.com>
2023-09-30 17:07:05 +0800

    fix Default Seeder

--- web/database/seeders/DefaultSeeder.php
+++ web/database/seeders/DefaultSeeder.php
@@ -21,12 +21,21 @@ class DefaultSeeder extends Seeder
             DB::beginTransaction(); // Start a database transaction
             // Assume not yet seeded, try seeding the database
             $group = new Groups();
-            $group->fill(['name' => 'Admins', 'describe' => 'Default seeded Admin group']);
-            $group->save();
+            if (Groups::where('name', '=', 'Admins')->exists()) {
+                $group = Groups::where('name', '=', 'Admins')->first();
+            } else {
+                $group->fill(['name' => 'Admins', 'describe' => 'Default seeded Admin group']);
+                $group->save();
+            }
             $admin_group_id = $group->id;
+
             $group = new Groups();
-            $group->fill(['name' => 'Demos', 'describe' => 'Default seeded Demo group']);
-            $group->save();
+            if (Groups::where('name', '=', 'Demos')->exists()) {
+                $group = Groups::where('name', '=', 'Demos')->first();
+            } else {
+                $group->fill(['name' => 'Demos', 'describe' => 'Default seeded Demo group']);
+                $group->save();
+            }
             $demo_group_id = $group->id;
 
             $perm_records = [];
@@ -47,22 +56,29 @@ class DefaultSeeder extends Seeder
                 'created_at' => $currentTimestamp,
                 'updated_at' => $currentTimestamp,
             ];
-
+            GroupPermissions::where('group_id', '=', $demo_group_id)->delete();
+            GroupPermissions::where('group_id', '=', $admin_group_id)->delete();
             GroupPermissions::insert($perm_records);
+            if (!User::where('name', '=', 'dev')->exists()) {
+                $user = new User();
+                $user->fill([
+                    'name' => 'dev',
+                    'email' => 'dev@chat.gai.tw',
+                    'email_verified_at' => now(),
+                    'password' => Hash::make('develope'),
+                    'group_id' => $admin_group_id,
+                ]);
+                $user->save();
+            } else {
+                User::where('name', '=', 'dev')->fill(['group_id' => $admin_group_id]);
+            }
 
-            $user = new User();
-            $user->fill([
-                'name' => 'dev',
-                'email' => 'dev@chat.gai.tw',
-                'email_verified_at' => now(),
-                'password' => Hash::make('develope'),
-                'group_id' => $admin_group_id
-            ]);
-            $user->save();
-            $demousers = \App\Models\User::factory(3)->create();
-            $demousers->each(function ($user) use ($demo_group_id) {
-                $user->update(['group_id' => $demo_group_id]);
-            });
+            if (!User::where('name', 'like', 'demo%')->exists()) {
+                $demousers = \App\Models\User::factory(3)->create();
+                $demousers->each(function ($user) use ($demo_group_id) {
+                    $user->update(['group_id' => $demo_group_id]);
+                });
+            }
             DB::commit();
         } catch (\Exception $e) {
             DB::rollBack(); // Rollback the transaction in case of an exception
