1049d92d192a101354396633f856e1881bc7f17d
MonkeyHuang <iijusuu0815110336@gmail.com>
2023-10-01 23:46:20 +0800

    update LLMs API in docker

---
+++ Docker/APIs/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+function cleanup() {
+    echo "Stopping Python program..."
+    kill -SIGTERM $PYTHON_PID
+    wait $PYTHON_PID
+    echo "Python program stopped."
+    exit
+}
+trap cleanup SIGTERM
+cd /API
+apt update
+apt install -y curl screen
+pip install -U transformers accelerate
+pip install safetensors protobuf
+while ! curl -s http://web:9000/v1.0/worker/debug >/dev/null; do
+  echo "Waiting for connection to http://web:9000/v1.0/worker/debug ..."
+  sleep 1
+done
+
+echo "Connected to http://web:9000/v1.0/worker/debug"
+export CUDA_VISIBLE_DEVICES=0 
+python3 dummy.py &
+PYTHON_PID=$!
+
+wait $PYTHON_PID
