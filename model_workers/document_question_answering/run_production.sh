23a1fde8565151c35e660af8d5413605af595332
Yung-Hsiang Hu <iftnt1999@gmail.com>
2023-10-12 03:09:53 +0800

    [DocQA] new: Docker related file for production

--- model_workers/document_question_answering/run_production.sh
+++ model_workers/document_question_answering/run_production.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+PORT=${PORT:-$(
+        python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()'
+    )}
+echo PORT=${PORT}
 docker compose -p doc-qa-${PORT} -f docker-compose-production.yml "$@"
