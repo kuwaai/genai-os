435f7079b88a5d335dce77761f1098a2d7ba1abb
黎サブ 星野 <taifu9920923@gmail.com>
2023-10-19 20:46:05 +0800

    fix

--- web/resources/views/layouts/app.blade.php
+++ web/resources/views/layouts/app.blade.php
@@ -39,7 +39,7 @@
                         <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
                             {{ __('System Announcement') }}
                         </h3>
-                        <button type="button" onclick="modal1.hide();"
+                        <button type="button" onclick="$modal1.hide();"
                             class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
                             data-modal-hide="system_announcement_modal">
                             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -61,7 +61,7 @@
                     <!-- Modal footer -->
                     <div
                         class="flex items-center p-4 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                        <button data-modal-hide="system_announcement_modal" type="button" onclick="modal1.hide();"
+                        <button data-modal-hide="system_announcement_modal" type="button" onclick="$modal1.hide();"
                             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{{ __('Close') }}</button>
                     </div>
                 </div>
@@ -92,7 +92,7 @@
                     <!-- Modal footer -->
                     <div
                         class="flex items-center p-4 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                        <button data-modal-hide="tos_modal" type="button" onclick="modal2.hide();"
+                        <button data-modal-hide="tos_modal" type="button" onclick="$modal2.hide();"
                             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{{ __('I accepted') }}</button>
                     </div>
                 </div>
@@ -119,37 +119,37 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
     <script>
         @if (\App\Models\SystemSetting::where('key', 'tos')->first()->value != '' && !Auth::user()->term_accepted)
-            const modal1 = new Modal(document.getElementById('system_announcement_modal'), {
+            $modal1 = new Modal(document.getElementById('system_announcement_modal'), {
                 backdrop: 'static',
                 closable: true,
                 onHide: () => {
                     $.get("{{ route('announcement') }}")
                 }
             });
-            const modal2 = new Modal(document.getElementById('tos_modal'), {
+            $modal2 = new Modal(document.getElementById('tos_modal'), {
                 backdrop: 'static',
                 closable: true,
                 onHide: () => {
                     $.get("{{ route('tos') }}")
 
-                    modal1.show();
+                    $modal1.show();
                 }
             });
-            modal2.show();
+            $modal2.show();
         @elseif (
             \App\Models\SystemSetting::where('key', 'announcement')->first()->value != '' &&
                 !\Illuminate\Support\Facades\Hash::check(
                     \App\Models\SystemSetting::where('key', 'announcement')->first()->value,
                     session()->get('announcement')))
 
-            const modal1 = new Modal(document.getElementById('system_announcement_modal'), {
+            $modal1 = new Modal(document.getElementById('system_announcement_modal'), {
                 backdrop: 'static',
                 closable: true,
                 onHide: () => {
                     $.get("{{ route('announcement') }}")
                 }
             });
-            modal1.show();
+            $modal1.show();
         @endif
     </script>
 </body>
