cfb70b90a173d6e249f70bceab8acd703e80830b
黎サブ 星野 <taifu9920923@gmail.com>
2023-10-21 15:02:23 +0800

    information added

--- web/resources/views/layouts/navigation.blade.php
+++ web/resources/views/layouts/navigation.blade.php
@@ -79,6 +79,8 @@
                             {{ __("Language") }}
                         </x-dropdown-link>
 
+                        <a class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out" href="https://www.taide.tw/" target="_blank">{{__("Information")}}</a>
+
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
@@ -156,7 +158,7 @@
                 <x-responsive-nav-link :href="route('lang')">
                     {{ __("Language") }}
                 </x-responsive-nav-link>
-                <!-- Authentication -->
+                <a class="block w-full pl-3 pr-4 py-2 border-l-4 border-transparent text-left text-base font-medium text-gray-600 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-700 hover:border-gray-300 dark:hover:border-gray-600 focus:outline-none focus:text-gray-800 dark:focus:text-gray-200 focus:bg-gray-50 dark:focus:bg-gray-700 focus:border-gray-300 dark:focus:border-gray-600 transition duration-150 ease-in-out" href="https://taide.tw/" target="_blank">{{__("Information")}}</a>
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
