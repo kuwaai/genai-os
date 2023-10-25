e0e70aa4b070dc479d6ad3d2443a3a4a4d853275
MonkeyHuang <iijusuu0815110336@gmail.com>
2023-10-25 15:43:58 +0800

    structural fit

---
+++ main.py
@@ -0,0 +1,43 @@
+# -#- coding: UTF-8 -*-
+import time, re, os, logging, click, requests
+from datetime import datetime
+from src.variable import *
+from src.functions import *
+from flask import Flask
+from flask_sse import ServerSentEventsBlueprint
+from routes.worker import worker
+from routes.chat import chat
+
+if __name__ == '__main__':
+    # Setup logfile location
+    init_folder(log_folder)
+    
+    
+    # Setup console logger and file logger
+    log_formatter = ANSIEscapeCodeRemovalHandler('[%(levelname)s] %(message)s')
+    console_handler = logging.StreamHandler()
+    file_handler = logging.FileHandler(log_file_path)
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(log_formatter)
+    logger = logging.getLogger()
+    logger.addHandler(file_handler)
+    console_handler.setFormatter(log_formatter)
+    console_handler.setLevel(logging.DEBUG)
+    logger.addHandler(console_handler)
+    
+    # Load savefile
+    if os.path.exists(record_file):
+        loadRecords(load_variable_from_file(record_file))
+    
+    # Init Flask Apps
+    app = Flask(__name__)
+    app.config["REDIS_URL"] = "redis://localhost:6379/0"
+    sse = ServerSentEventsBlueprint('sse', __name__)
+    app.register_blueprint(sse, url_prefix='/')
+    app.register_blueprint(worker, url_prefix=f'/{version}/worker')
+    app.register_blueprint(chat, url_prefix=f'/{version}/chat')
+    log(0,"Route list:\n","\n".join([str(i) for i in app.url_map.iter_rules()]), "\n")
+    log(0,"Server started")
+    app.run(port=9000, host="0.0.0.0", threaded=True)
+    #Stopped, saving to file
+    save_variable_to_file(record_file, data)
---
+++ src/functions.py
@@ -0,0 +1,60 @@
+import os, logging, re, gzip, pickle, requests
+from urllib.parse import urlparse
+from flask import make_response
+from json import dumps
+from src.variable import *
+
+class ANSIEscapeCodeRemovalHandler(logging.Formatter):
+    ANSI_ESCAPE = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
+
+    def format(self, record):
+        message = super().format(record)
+        if re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - - \[\d{2}/[A-Za-z]{3}/\d{4} \d{2}:\d{2}:\d{2}\] "[^"]*" \d{3} -',message):
+            message = re.sub(r'\[\d{1,2}/[A-Za-z]{3}/\d{4} \d{2}:\d{2}:\d{2}\]', "", message).replace(" - -  ", " -> ")[:-2]
+        return f"[{datetime.now().strftime('%Y-%m-%d %H-%M-%S.%f')}]" + self.ANSI_ESCAPE.sub('', message)
+
+def init_folder(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+def log(state, *args, **kwargs):
+    log_level = ["INFO", "WARNING", "CRITIAL"]
+    message = ' '.join(map(str, args))
+    msg = f"[{datetime.now().strftime('%Y-%m-%d %H-%M-%S.%f')}][{log_level[state]}] {message}"
+    print(msg)
+    with open(log_file_path, "a") as file:
+        file.write(msg + "\n")
+
+def save_variable_to_file(filename, data):
+    with gzip.open(filename, 'wb') as file:
+        pickle.dump(data, file, protocol=pickle.HIGHEST_PROTOCOL)
+    log(0,"Records saved\n",data)
+
+def load_variable_from_file(filename):
+    with gzip.open(filename, 'rb') as file:
+        return pickle.load(file)
+
+def endpoint_formatter(endpoint):
+    return endpoint[:-1] if endpoint.endswith("/") else endpoint
+        
+        
+def get_base_url(url):
+    parsed_url = urlparse(url)
+    base_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
+    return base_url
+    
+def loadRecords(var, keep_state = False):
+    log(0,"Loading records, Here's before\n",data)
+    for i,o in var.items():
+        data[i] = []
+        for k in o:
+            try:
+                resp = requests.get(get_base_url(k[0]) + "/health")
+                if resp.status_code == 204:
+                    data[i].append(k if keep_state else [endpoint_formatter(k[0]),"READY",-1,-1])
+                else:
+                    log(0,f"Healthy check failed in {i} at {k[0]}, removed")
+            except requests.exceptions.ConnectionError as e:
+                log(0,f"Healthy check failed in {i} at {k[0]}, removed")
+        if len(data[i]) == 0: del data[i]
+    log(0,"Records loaded, Here's After\n",data)
