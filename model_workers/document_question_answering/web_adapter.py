abb0f9eb184d4f8232a3ffa55d5e751e503e35ae
Yung-Hsiang Hu <iftnt1999@gmail.com>
2023-10-27 22:00:36 +0800

    [DocQA] new: SearchQA
    SearchQA utilizes the search engine to retrieve URLs related to user
    questions, then uses WebQA to scrape them and answer the question.
    
    This patch contributes to the following.
    1. The basic logic of SearchQA with Google's searching API
    2. Extend the WebQA logic to accept multiple URLs

--- model_workers/document_question_answering/web_adapter.py
+++ model_workers/document_question_answering/web_adapter.py
@@ -9,6 +9,10 @@ from docqa.docqa import DocumentQa
 import re
 import logging
 import asyncio
+import os
+import functools
+import itertools
+import requests
 
 class NoUrlException(Exception):
   def __str__(self):
@@ -45,7 +49,7 @@ class DocumentQaProcess(GeneralProcessInterface):
       if url == None : raise NoUrlException
       
       chat_history = [ChatRecord(msg=None, role=Role.USER)] + chat_history[1:]
-      async for reply in self.app.process(url, chat_history):
+      async for reply in self.app.process([url], chat_history):
         yield reply
 
     except NoUrlException as e:
@@ -75,6 +79,84 @@ class DatabaseQaProcess(GeneralProcessInterface):
 
       async for reply in self.app.process(None, chat_history):
         yield reply
+
+    except Exception as e:
+      await asyncio.sleep(2) # To prevent SSE error of web page.
+      self.logger.exception('Unexpected error')
+      yield '發生錯誤，請再試一次或是聯絡管理員。'
+
+class SearchQaProcess(GeneralProcessInterface):
+  def __init__(self):
+    self.logger = logging.getLogger(__name__)
+    self.app = DocumentQa()
+
+  async def is_url_reachable(self, url:str, timeout=5) -> bool:
+    loop = asyncio.get_running_loop()
+    resp = None
+    try:
+      resp = await loop.run_in_executor(
+        None,
+        functools.partial(
+          requests.get,
+          url,
+          timeout=timeout
+        )
+      )
+    except Exception as e:
+      self.logger.exception(e)
+      pass
+    finally:
+      return resp != None and resp.ok
+
+  async def search_url(self, chat_history: [ChatRecord], num_url = 3) -> str:
+    """
+    Get first URL from the search result.
+    """
+
+    api_key = os.environ['GOOGLE_API_KEY']
+    searching_engine_id = os.environ['GOOGLE_CSE_ID']
+    latest_user_record = next(filter(lambda x: x.role == Role.USER, reversed(chat_history)))
+    latest_user_msg = latest_user_record.msg
+    
+    endpoint = 'https://customsearch.googleapis.com/customsearch/v1'
+    params = {
+      'key': api_key,
+      'cx': searching_engine_id,
+      'q': latest_user_msg
+    }
+
+    loop = asyncio.get_running_loop()
+    resp = await loop.run_in_executor(
+      None,
+      functools.partial(
+        requests.get,
+        endpoint,
+        params = params
+      )
+    )
+    self.logger.debug(f'Search response ({resp.status_code}):\n{resp.content}')
+
+    resp  = resp.json()
+    urls = [item['link'] for item in resp['items']]
+    urls_reachable = await asyncio.gather(*[self.is_url_reachable(url) for url in urls])
+    self.logger.debug(list(zip(urls, urls_reachable)))
+    urls = list(itertools.compress(urls, urls_reachable))
+    urls = urls[:min(len(urls), num_url)]
+
+    return urls, [latest_user_record]
+
+  async def process(self, chat_history: [ChatRecord]) -> Generator[str, None, None]:
+
+    try:
+    
+      urls, chat_history = await self.search_url(chat_history)
+      
+      async for reply in self.app.process(urls, chat_history):
+        yield reply
+      
+      yield f'\n\n參考資料：\n'
+      for url in urls:
+        yield f'{url}\n'
 
     except Exception as e:
       await asyncio.sleep(2) # To prevent SSE error of web page.
