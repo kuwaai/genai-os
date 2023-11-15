2ffeb286f03633e4a38477d0be41fbeeb880335d
黎サブ 星野 <taifu9920923@gmail.com>
2023-11-15 17:56:45 +0800

    Added delete confirm to Duel

--- web/resources/views/duel.blade.php
+++ web/resources/views/duel.blade.php
@@ -310,6 +310,50 @@
                 {{ __('Select a chatroom to begin with') }}
             </div>
         @else
+            @if (!session('llms'))
+                <div id="delete_chat_modal" tabindex="-1"
+                    class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                    <div class="relative w-full max-w-md max-h-full">
+                        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                            <button type="button"
+                                class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                data-modal-hide="delete_chat_modal">
+                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                    fill="none" viewBox="0 0 14 14">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                </svg>
+                                <span class="sr-only">Close modal</span>
+                            </button>
+                            <form id="deleteChat" action="{{ route('duel.delete') }}" method="post"
+                                class="p-6 text-center">
+                                <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200"
+                                    aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                    viewBox="0 0 20 20">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                </svg>
+                                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                                    {{ __('Are you sure you want to DELETE the chat ') }}"<span>{{ App\Models\DuelChat::findOrFail(request()->route('duel_id'))->name }}</span>"?
+                                </h3>
+                                @csrf
+                                @method('delete')
+                                <input name="id" type="hidden"
+                                    value="{{ App\Models\DuelChat::findOrFail(request()->route('duel_id'))->id }}" />
+                                <input type="hidden" name="limit"
+                                    value="{{ request()->input('limit') > 0 ? request()->input('limit') : '0' }}">
+                                <button type="submit"
+                                    class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                                    {{ __('Delete') }}
+                                </button>
+                                <button data-modal-hide="delete_chat_modal" type="button"
+                                    class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">{{ __('Cancel') }}</button>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            @endif
             <div id="feedback" data-modal-backdrop="static" tabindex="-1" aria-hidden="true"
                 class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
                 <div class="relative w-full max-w-2xl max-h-full">
@@ -394,15 +438,6 @@
             <div id="histories"
                 class="flex-1 h-full flex flex-col w-full bg-gray-200 dark:bg-gray-600 shadow-xl rounded-r-lg overflow-hidden">
                 @if (!session('llms'))
-                    <form id="deleteChat" action="{{ route('duel.delete') }}" method="post" class="hidden">
-                        @csrf
-                        @method('delete')
-                        <input name="id"
-                            value="{{ App\Models\DuelChat::findOrFail(request()->route('duel_id'))->id }}" />
-                        <input type="hidden" name="limit"
-                            value="{{ request()->input('limit') > 0 ? request()->input('limit') : '0' }}">
-                    </form>
-
                     <form id="editChat" action="{{ route('duel.edit') }}" method="post" class="hidden">
                         @csrf
                         <input name="id"
@@ -474,7 +509,7 @@
                                 class="bg-orange-500 hover:bg-orange-600 text-white font-bold py-3 px-4 rounded flex items-center justify-center">
                                 <i class="fas fa-pen"></i>
                             </button>
-                            <button onclick="deleteChat()"
+                            <button data-modal-target="delete_chat_modal" data-modal-toggle="delete_chat_modal"
                                 class="bg-red-500 ml-3 hover:bg-red-600 text-white font-bold py-3 px-4 rounded flex items-center justify-center">
                                 <i class="fas fa-trash"></i>
                             </button>
