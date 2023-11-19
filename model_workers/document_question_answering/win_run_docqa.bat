10ac95a5322ac282e98087f42f4c3d178788ca54
黎サブ 星野 <taifu9920923@gmail.com>
2023-11-20 03:34:49 +0800

    Update

--- model_workers/document_question_answering/win_run_docqa.bat
+++
@@ -1,9 +0,0 @@
-set LAYOUT_CONFIG=./layout.yaml
-set MODEL_LOCATION=remote-llmproject
-set LLM_NAME=doc_qa
-set access_code=model
-set AGENT_ENDPOINT=http://127.0.0.1:9000/v1.0/
-set PUBLIC_ADDRESS=127.0.0.1
-set llm_log_path=./doc.jsonl
-set taide_api_token=b712d9ba6fc3f9d7c37c6fb0877cc50c3340661110576ac894facfd177c0dc3f
-worker-server
