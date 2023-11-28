ccaa7427c22c41c784d159cd597a36fe0748c7ef
黎サブ 星野 <taifu9920923@gmail.com>
2023-11-28 09:18:57 +0800

    Change NUK logo

--- web/resources/views/welcome.blade.php
+++ web/resources/views/welcome.blade.php
@@ -62,7 +62,7 @@
                 <h3 class="text-5xl font-bold mb-2 text-blue-600 dark:text-cyan-200">
                     <div class="flex items-center justify-center overflow-hidden">
                         <a class="rounded-full overflow-hidden" href="https://www.csie.nuk.edu.tw/" target="_blank">
-                            <img class="w-[150px]" src="{{ asset('images/csie_logo.svg') }}">
+                            <img class="w-[150px]" src="{{ asset('images/csie.png') }}">
                         </a>
                         <div class="flex flex-col ml-4 text-[50px]">
                             <span>LLM</span>
