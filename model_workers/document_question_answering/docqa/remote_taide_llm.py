24b64405fe3b394b1fcab98e5c6f1d410c14d0fa
Yung-Hsiang Hu <iftnt1999@gmail.com>
2023-12-22 22:32:06 +0800

    [DocQA] refactor: The remote API of LLM project.
    1. Refactor the API invoker to the LLM project.
    2. Change the tokenizer to AutoTokenizer to fit newer models.

--- model_workers/document_question_answering/docqa/remote_taide_llm.py
+++ model_workers/document_question_answering/docqa/remote_taide_llm.py
@@ -2,11 +2,13 @@ import asyncio
 import requests
 import functools
 import logging
+import json
 from os import environ
 from oauthlib.oauth2 import LegacyApplicationClient
 from requests_oauthlib import OAuth2Session
 
 from .taide_llm import TaideLlm
+from worker_framework.datatype import ChatRecord, Role
 
 logger = logging.getLogger(__name__)
 
@@ -109,3 +111,72 @@ class NchcTaideLlm(TaideLlm):
             raise
         finally:
             return result, output_tokens
+
+class LlmProjectTaideLlm(TaideLlm):
+    def __init__(self):
+        super(LlmProjectTaideLlm, self).__init__()
+        self.api_endpoint = environ.get(
+            'LLM_PROJECT_TAIDE_API_ENDPOINT',
+            'https://chatdev.gai.tw/v1.0/chat/completions'
+        )
+        assert 'LLM_PROJECT_TAIDE_API_TOKEN' in environ, 'Environment variable "LLM_PROJECT_TAIDE_API_TOKEN" is not set to use the LLM_PROJECT TAIDE API.'
+        assert 'LLM_PROJECT_TAIDE_ACCESS_CODE' in environ, 'Environment variable "LLM_PROJECT_TAIDE_ACCESS_CODE" is not set to use the LLM_PROJECT TAIDE API.'
+        self.api_token = environ['LLM_PROJECT_TAIDE_API_TOKEN']
+        self.model_access_code = environ['LLM_PROJECT_TAIDE_ACCESS_CODE']
+
+    async def call_api(self, messages: [dict]):
+        # Define the API endpoint and authentication headers.
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {self.api_token}'
+        }
+        logger.debug("------------------")
+        logger.debug(f"Using access_code: {self.model_access_code}")
+        logger.debug(f"Input messages: {messages}")
+        logger.debug("------------------")
+
+        request_data = {
+            "messages": messages,
+            "model": self.model_access_code
+        }
+        
+        # Perform the HTTP request.
+        loop = asyncio.get_running_loop()
+        response = await loop.run_in_executor(
+            None,
+            functools.partial(
+                requests.post,
+                self.api_endpoint,
+                headers=headers,
+                json=request_data
+            )
+        )
+        logger.debug(response.json())
+        if not response.ok:
+            raise RuntimeError(f'Request failed with status {response.status_code}')
+        
+        data = response.json()
+        return data["output"]
+
+    async def complete(self, chat_history: [ChatRecord]) -> str:
+        chat_history = [
+            {
+                'isbot': bool(record.role != Role.USER),
+                'msg': record.msg
+            }
+            for record in chat_history
+        ]
+        
+        result = ''
+        try:
+            result = await self.call_api(chat_history)
+            logger.debug(f'Reply from API: {result}')
+            return result
+            
+        except Exception as e:
+            result = ''
+            logger.exception('Generation failed.')
+            raise
+        
+    async def _complete(self, prompt:list, tokens:int)-> (str, int):
+        raise NotImplementedError()
