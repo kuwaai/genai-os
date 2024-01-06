f11169c3b59e2f404796071afa47c6c928d892b4
黎サブ 星野 <taifu9920923@gmail.com>
2024-01-07 00:53:39 +0800

    basic upgrade to agent added guard and async flag

--- src/variable.py
+++ src/variable.py
@@ -5,5 +5,9 @@ version = "v1.0"
 data = {}
 log_folder = "logs"
 record_file = "records.pickle"
+safety_guard = "safety-guard"
+
+port = 9000
+ip = "0.0.0.0"
 
 log_file_path = os.path.join(log_folder, datetime.now().strftime('%Y-%m-%d_%H-%M-%S.%f') + '.log')
