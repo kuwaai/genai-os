4b1b6d5e5ad4397bd3d04a17d50d1d633f1b2162
黎サブ 星野 <taifu9920923@gmail.com>
2024-01-18 08:36:43 +0800

    revert

--- web/resources/views/components/chat/react-buttons.blade.php
+++ web/resources/views/components/chat/react-buttons.blade.php
@@ -1,5 +1,3 @@
-<!-- resources/views/components/ButtonGroupComponent.blade.php -->
-
 @props(['history', 'showOnFinished'])
 
 <div class="flex space-x-1{{ $showOnFinished ? ' show-on-finished' : '' }}"
@@ -78,7 +76,7 @@
             request()->user()->hasPerm('Chat_update_react_message')) ||
             (request()->routeIs('room.*') &&
                 request()->user()->hasPerm('Room_update_react_message')))
-        <!--<button data-tooltip-target="react_translate" data-tooltip-placement="top"
+        <button data-tooltip-target="react_translate" data-tooltip-placement="top"
             onclick="translates(this, {{ $history->id }}, null)"
             class="flex text-black hover:bg-gray-400 p-2 h-[32px] w-[32px] justify-center items-center rounded-lg translates">
             <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" version="1.0">
@@ -131,6 +129,6 @@
                 <path
                     d="M504 256c0 137-111 248-248 248S8 393 8 256C8 119.1 119 8 256 8s248 111.1 248 248zm-248 50c-25.4 0-46 20.6-46 46s20.6 46 46 46 46-20.6 46-46-20.6-46-46-46zm-43.7-165.3l7.4 136c.3 6.4 5.6 11.3 12 11.3h48.5c6.4 0 11.6-5 12-11.3l7.4-136c.4-6.9-5.1-12.7-12-12.7h-63.4c-6.9 0-12.4 5.8-12 12.7z" />
             </svg>
-        </button>-->
+        </button>
     @endif
 </div>
