36ea391a6e50ff127b842ac9f2012c99b834a2b3
黎サブ 星野 <taifu9920923@gmail.com>
2024-01-22 21:40:56 +0800

    Fix group create bug

--- web/resources/views/manage/tabs/groups.blade.php
+++ web/resources/views/manage/tabs/groups.blade.php
@@ -195,44 +195,42 @@
                         @endforeach
                     </div>
                 </div>
-                <div class="w-full px-3 mb-3">
-                    <div class="w-full mb-3 mt-2">
-                        <span
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('Model Permissions (Enabled LLM)') }}</span>
-                        <div
-                            class="grid gap-3 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 p-2 border border-gray-500 dark:border-gray-200 rounded-lg dark:border-white">
-                            @foreach (DB::table(function ($query) {
+                <div class="w-full mb-3 mt-2">
+                    <span
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('Model Permissions (Enabled LLM)') }}</span>
+                    <div
+                        class="grid gap-3 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 p-2 border border-gray-500 dark:border-gray-200 rounded-lg dark:border-white">
+                        @foreach (DB::table(function ($query) {
         $query->select(DB::raw('substring(name, 7) as model_id, id'))->from('permissions')->where('name', 'like', 'model_%');
     }, 'p')->join('llms', 'llms.id', '=', DB::raw('p.model_id::bigint'))->where('enabled', '=', true)->select('p.id as id', 'llms.name as name')->orderby('created_at', 'desc')->get() as $LLM)
-                                <div
-                                    class="flex items-center pl-4 border border-gray-500 dark:border-gray-200 rounded-lg dark:border-white">
-                                    <input id="edit_checkbox_{{ $LLM->id }}" type="checkbox"
-                                        value="{{ $LLM->id }}" name="permissions[]"
-                                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded dark:bg-gray-700 dark:border-gray-600"
-                                        style="box-shadow:none;"> <label for="edit_checkbox_{{ $LLM->id }}"
-                                        class="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-white">{{ $LLM->name }}</label>
-                                </div>
-                            @endforeach
-                        </div>
+                            <div
+                                class="flex items-center pl-4 border border-gray-500 dark:border-gray-200 rounded-lg dark:border-white">
+                                <input id="create_checkbox_{{ $LLM->id }}" type="checkbox"
+                                    value="{{ $LLM->id }}" name="permissions[]"
+                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded dark:bg-gray-700 dark:border-gray-600"
+                                    style="box-shadow:none;"> <label for="create_checkbox_{{ $LLM->id }}"
+                                    class="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-white">{{ $LLM->name }}</label>
+                            </div>
+                        @endforeach
                     </div>
-                    <div class="w-full mb-3">
-                        <span
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('Model Permissions (Disabled LLM)') }}</span>
-                        <div
-                            class="grid gap-3 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 p-2 border border-gray-500 dark:border-gray-200 rounded-lg dark:border-white">
-                            @foreach (DB::table(function ($query) {
+                </div>
+                <div class="w-full mb-3">
+                    <span
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('Model Permissions (Disabled LLM)') }}</span>
+                    <div
+                        class="grid gap-3 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 p-2 border border-gray-500 dark:border-gray-200 rounded-lg dark:border-white">
+                        @foreach (DB::table(function ($query) {
         $query->select(DB::raw('substring(name, 7) as model_id, id'))->from('permissions')->where('name', 'like', 'model_%');
     }, 'p')->join('llms', 'llms.id', '=', DB::raw('p.model_id::bigint'))->where('enabled', '=', false)->select('p.id as id', 'llms.name as name')->orderby('created_at', 'desc')->get() as $LLM)
-                                <div
-                                    class="flex items-center pl-4 border border-gray-500 dark:border-gray-200 rounded-lg dark:border-white">
-                                    <input id="edit_checkbox_{{ $LLM->id }}" type="checkbox"
-                                        value="{{ $LLM->id }}" name="permissions[]"
-                                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded dark:bg-gray-700 dark:border-gray-600"
-                                        style="box-shadow:none;"> <label for="edit_checkbox_{{ $LLM->id }}"
-                                        class="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-white">{{ $LLM->name }}</label>
-                                </div>
-                            @endforeach
-                        </div>
+                            <div
+                                class="flex items-center pl-4 border border-gray-500 dark:border-gray-200 rounded-lg dark:border-white">
+                                <input id="create_checkbox_{{ $LLM->id }}" type="checkbox"
+                                    value="{{ $LLM->id }}" name="permissions[]"
+                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded dark:bg-gray-700 dark:border-gray-600"
+                                    style="box-shadow:none;"> <label for="create_checkbox_{{ $LLM->id }}"
+                                    class="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-white">{{ $LLM->name }}</label>
+                            </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
