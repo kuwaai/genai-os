3138457299f319e3ea9bb49c6859e6d8e0163f4a
黎サブ 星野 <taifu9920923@gmail.com>
2024-01-26 04:21:37 +0800

    final warning style

--- web/resources/views/components/chat/functions.blade.php
+++ web/resources/views/components/chat/functions.blade.php
@@ -40,13 +40,13 @@ fill="currentFill" />
                     if (warnings) {
                         warnings = warnings[1].split("\n")
                         var listItems = warnings.map(function(line) {
-                            return `<div class="warning_msg mt-2 flex items-center p-4 text-sm text-gray-800 rounded-lg bg-gray-50 dark:bg-gray-800 dark:text-gray-300" role="alert">
+                            return `<div class="warning_msg mt-2 flex items-center p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 role="alert">
   <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
     <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
   </svg>
   <span class="sr-only">Info</span>
   <div class="ml-2">
-    <span class="font-medium">` + line + `</span>
+    <span class="font-medium">`+line+`</span>
   </div>
 </div>`;
                         });
