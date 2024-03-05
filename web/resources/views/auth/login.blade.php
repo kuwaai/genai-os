efc292497ff0700f06aa59b5fb348fc29538ebbe
黎サブ 星野 <taifu9920923@gmail.com>
2024-03-05 13:23:50 +0800

    re-enable: - link share, - safety guard react button - translate react button - abort button - forgot password button

--- web/resources/views/auth/login.blade.php
+++ web/resources/views/auth/login.blade.php
@@ -33,11 +33,11 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <!--@if (Route::has('password.request'))
+            @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
-            @endif-->
+            @endif
 
             <x-primary-button class="ml-3">
                 {{ __('Log in') }}
