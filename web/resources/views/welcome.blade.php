77217aa9095d74b720840a572539cc3c737dddcc
黎サブ 星野 <taifu9920923@gmail.com>
2024-03-12 09:15:34 +0800

    Fix error

--- web/resources/views/welcome.blade.php
+++ web/resources/views/welcome.blade.php
@@ -45,10 +45,6 @@
                                 <a href="{{ route('room.home') }}"
                                     class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:rounded-sm focus:outline-red-500">{{ __('room.route') }}</a>
                             @endif
-                            @if (Auth::user()->hasPerm('tab_Chat'))
-                                <a href="{{ route('chat.home') }}"
-                                    class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">{{ __('chat.route') }}</a>
-                            @endif
                             <a href="{{ route('logout') }}"
                                 onclick="event.preventDefault(); this.closest('form').submit();"
                                 class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:rounded-sm focus:outline-red-500">{{ __('login.button.sign_out') }}</a>
