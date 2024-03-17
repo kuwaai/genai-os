49ba26a1dc46e7c265e97cd9bfb2c3b540f67b5c
黎サブ 星野 <taifu9920923@gmail.com>
2024-03-18 03:25:15 +0800

    HSTS fix

--- web/hsts.php
+++ web/hsts.php
@@ -2,4 +2,4 @@
 //HSTS already set for production, 
 //but checkmarx still giving errors, 
 //so I'm putting this line to prevent HSTS warning
-header("Strict-Transport-Security: max-age=36000; includeSubDomains"); ?>
+header("Strict-Transport-Security: max-age=31536000; includeSubDomains") ?>
