ed809c83063642c5a25a45648ba6403c4eacfd01
黎サブ 星野 <taifu9920923@gmail.com>
2024-03-18 01:52:48 +0800

    add HSTS

--- web/config/app.php
+++ web/config/app.php
@@ -63,7 +63,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://localhost'),
 
     'asset_url' => env('ASSET_URL'),
 
@@ -203,6 +203,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Bepsvpt\SecureHeaders\SecureHeadersServiceProvider::class,
 
     ],
 
