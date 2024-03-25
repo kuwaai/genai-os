112855178439092113a1ec79511e19fb3a6bf6ec
黎サブ 星野 <taifu9920923@gmail.com>
2024-03-25 16:24:59 +0800

    fix files

--- multi-chat/LLMs/llms/chatgpt.py
+++ multi-chat/LLMs/llms/chatgpt.py
@@ -1,29 +1,8 @@
 import socket, os, openai
 from base import *
 
-<<<<<<< HEAD:multi-chat/LLMs/llms/chatgpt.py
-# -- Configs --
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-app.agent_endpoint = "http://127.0.0.1:9000/"
-app.LLM_name = "chatgpt"
-app.version_code = "v1.0"
-app.ignore_agent = False
-# This is the IP that will be stored in Agent, Make sure the IP address here are accessible by Agent
-public_ip = None
-if public_ip == None: public_ip = socket.gethostbyname(socket.gethostname())
-# The port to use, by choosing None, it'll assign an unused port
-app.port = None 
-if app.port == None:
-    with socket.socket() as s:
-        app.port = s.bind(('', 0)) or s.getsockname()[1]
-path = "/"
-app.reg_endpoint = f"http://{public_ip}:{app.port}{path}"
-limit = 1024*2
-# -- Config ends --
-=======
 if not app.LLM_name:
     app.LLM_name = "chatgpt"
->>>>>>> 0cbbb60a4f1bce269c45504f8d6008ef1cb1e4d1:LLMs/llms/chatgpt.py
 
 global proc
 proc = False
