4e8fa36f1b06b2552dc88394ca7ba94d1619a26e
黎サブ 星野 <taifu9920923@gmail.com>
2024-04-03 03:47:24 +0800

    Fix windows

--- windows/workers/geminipro/init.bat
+++ windows/workers/geminipro/init.bat
@@ -14,7 +14,7 @@ set "names[5]=Custom Module"
 REM Define an array to store the model types and their names
 set "models[1]=chatgpt"
 set "models[2]=geminipro"
-set "models[3]=gguf"
+set "models[3]=llamacpp"
 set "models[4]=huggingface"
 set "models[5]=custom"
 
@@ -42,7 +42,7 @@ for %%a in (1 2 3 4 5) do (
 
 REM Ask for user input
 :input_option
-set /p "option=Enter the option number (1-4): "
+set /p "option=Enter the option number (1-5): "
 if not defined models[%option%] (
     echo Invalid option. Please try again.
     goto input_option
@@ -50,6 +50,16 @@ if not defined models[%option%] (
 
 REM Set the model type based on the selected option
 set "model_type=!models[%option%]!"
+
+if "!option!" == "5" (
+    REM Ask for worker path (must-fill field)
+    :input_worker_path
+    set /p "worker_path=Enter the worker path: "
+    if "!worker_path!"=="" (
+        echo Worker path cannot be blank. Please try again.
+        goto input_worker_path
+    )
+)
 
 REM Ask for model name
 :input_model_name
@@ -84,8 +94,15 @@ if "!model_type!"=="geminipro" (
 
 :continue
 
-REM Ask for model path if the model type is GGUF or Hugging Face
-if "!model_type!"=="gguf" (
+REM Ask for model path if the model type is llamacpp or Hugging Face
+if "!model_type!"=="llamacpp" (
+	for /r %%i in (*.gguf) do (
+		echo "using founded .gguf file"
+		echo model_path=%%~fi
+		set "model_path=%%~fi"
+		goto done
+	)
+
     :input_model_path
     set /p "model_path=Enter the model path: "
     if "!model_path!"=="" (
@@ -93,6 +110,12 @@ if "!model_type!"=="gguf" (
         goto input_model_path
     )
 ) else if "!model_type!"=="huggingface" (
+	for /r %%i in (*.model *.bin *.safetensor) do (
+		echo "model folder detected, using current folder path"
+		echo model_path=%%~dpi
+		set "model_path=%%~dpi"
+		goto done
+	)
     :input_model_path
     set /p "model_path=Enter the model path: "
     if "!model_path!"=="" (
@@ -101,12 +124,15 @@ if "!model_type!"=="gguf" (
     )
 )
 
+:done
+
 REM Save to env.bat
 echo set "model_type=!model_type!" > env.bat
 echo set "model_name=!model_name!" >> env.bat
 if defined api_key echo set "api_key=!api_key!" >> env.bat
 echo set "access_code=!access_code!" >> env.bat
 if defined model_path echo set "model_path=!model_path!" >> env.bat
+if defined worker_path echo set "worker_path=!worker_path!" >> env.bat
 
 echo Configuration saved to env.bat
 pause
