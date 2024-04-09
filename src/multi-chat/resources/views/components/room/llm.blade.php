701df4d576d872bdd5c70087773756a4695fe049
黎サブ 星野 <taifu9920923@gmail.com>
2024-04-10 04:54:31 +0800

    Fix bugs

--- src/multi-chat/resources/views/components/room/llm.blade.php
+++ src/multi-chat/resources/views/components/room/llm.blade.php
@@ -30,6 +30,9 @@
     $DCs = $DCs->get()->groupBy('identifier');
 @endphp
 @foreach ($DCs as $DC)
+    @if (array_diff(explode(',', $DC->first()->identifier), $result->pluck('id')->toArray()) == [])
+
+
     <div class="mb-2 border border-black dark:border-white border-1 rounded-lg">
         @if (request()->user()->hasPerm('Room_update_new_chat'))
             <form method="post" action="{{ route('room.new') }}">
@@ -37,9 +40,9 @@
                 <button
                     class="flex px-2 scrollbar rounded-t-lg w-full hover:bg-gray-300 dark:hover:bg-gray-700 scrollbar-3 overflow-x-auto py-3 border-b border-black dark:border-white">
                     @foreach (App\Models\Chats::join('bots', 'bots.id', '=', 'bot_id')->Join('llms', function ($join) {
-                        $join->on('llms.id', '=', 'bots.model_id');
-                    })->where('user_id', Auth::user()->id)->where('roomID', $DC->first()->id)->orderby('bot_id')->get() as $chat)    
-                    <div
+            $join->on('llms.id', '=', 'bots.model_id');
+        })->where('user_id', Auth::user()->id)->where('roomID', $DC->first()->id)->orderby('bot_id')->get() as $chat)
+                        <div
                             class="mx-1 flex-shrink-0 h-5 w-5 rounded-full border border-gray-400 dark:border-gray-900 bg-black flex items-center justify-center overflow-hidden">
                             <div class="h-full w-full"><img data-tooltip-target="llm_{{ $chat->bot_id }}"
                                     data-tooltip-placement="top" class="h-full w-full"
@@ -52,7 +55,7 @@
                             </div>
                             <input name="llm[]" value="{{ $chat->bot_id }}" style="display:none;">
                         </div>
-                        @endforeach
+                    @endforeach
                 </button>
             </form>
         @else
@@ -118,4 +121,5 @@
             @endforeach
         </div>
     </div>
+    @endif
 @endforeach
