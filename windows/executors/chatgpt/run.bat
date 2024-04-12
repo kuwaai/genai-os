703a1d39a885d1d1346d0bda9d7167da073b0941
黎サブ 星野 <taifu9920923@gmail.com>
2024-04-13 06:02:35 +0800

    remove run.bat

--- windows/executors/chatgpt/run.bat
+++
@@ -1,5 +0,0 @@
-set EXECUTOR_ACCESS_CODE=chatgpt
-pushd ..\..\..\src\multi-chat
-php artisan model:config "chatgpt" "ChatGPT" --image "..\..\windows\executors\chatgpt\chatgpt.png"
-popd
-start /b "" "kuwa-executor" "chatgpt" "--access_code" "chatgpt"
--- windows/executors/geminipro/run.bat
+++
@@ -1,5 +0,0 @@
-set EXECUTOR_ACCESS_CODE=geminipro
-pushd ..\..\..\src\multi-chat
-php artisan model:config "geminipro" "Gemini Pro" --image "..\..\windows\executors\geminipro\geminipro.png"
-popd
-start /b "" "kuwa-executor" "geminipro" "--access_code" "geminipro"
