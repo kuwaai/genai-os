54eac23af45536b3473e19294fc6df372c893995
黎サブ 星野 <taifu9920923@gmail.com>
2024-04-29 14:19:07 +0800

    add tool for CPU and GPU

--- windows/tool.bat
+++ windows/tool.bat
@@ -1,10 +1,11 @@
 REM Loop to wait for commands
 @echo off
 call src\variables.bat
+setlocal enabledelayedexpansion
 set "PATH=%~dp0packages\%python_folder%;%~dp0packages\%python_folder%\Scripts;%PATH%"
 :loop
 set userInput=
-set /p userInput=Enter a command (quit, seed, hf login, prune, stop, cmd): 
+set /p userInput=Enter a command (quit, seed, hf login, prune, switch, stop, cmd): 
 
 if /I "%userInput%"=="quit" (
 	echo Quit.
@@ -24,6 +25,14 @@ if /I "%userInput%"=="quit" (
     echo Opening cmd
     call cmd
     goto loop
+) else if /I "%userInput%"=="switch" (
+	set version=
+	set /p version=Switch to version:
+	echo !version!
+    pushd src
+	call switch.bat !version!
+	popd
+    goto loop
 ) else if /I "%userInput%"=="prune" (
     echo Running prune command...
 	pushd ..\src\multi-chat\
@@ -33,3 +42,4 @@ if /I "%userInput%"=="quit" (
 ) else (
     goto loop
 )
+endlocal
