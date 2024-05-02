b7c74b7ec1322bbeb3fef98b710412adc439de7f
黎サブ 星野 <taifu9920923@gmail.com>
2024-05-03 07:44:41 +0800

    Fix some translation in English

--- src/multi-chat/lang/en_us/store.php
+++ src/multi-chat/lang/en_us/store.php
@@ -6,13 +6,13 @@ return [
     'label.offical_bots' => 'Offical Bots',
     'hint.must_select_base_model' => 'You must select a base model',
     'label.community_bots' => 'Community Bots',
-    'bot.startup_prompt' => 'Startup Prompt',
+    'bot.system_prompt' => 'Startup Prompt',
     'bot.welcome_message' => 'Welcome message',
     'bot.name' => 'Name',
     'bot.name.label' => 'Bot Name',
     'bot.description' => 'Description',
     'bot.description.label' => 'Bot Description',
-    'bot.label.startup_prompt' => 'The prompts will be inserted at the beginning of chatroom.',
-    'bot.label.welcome_message' => 'Welcome message that shows but won\'t be passed inside when chatting.',
+    'bot.system_prompt.label' => 'The prompts will be inserted at the beginning of chatroom.',
+    'bot.welcome_message.label' => 'Welcome message that shows but won\'t be passed inside when chatting.',
     'bot.button.create'=>"Create"
 ];
