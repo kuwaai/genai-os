2f47b5cedeb656687507de7ed1960202d8d064a1
黎サブ 星野 <taifu9920923@gmail.com>
2024-05-03 07:30:56 +0800

    Added helper for modelfile when editing bot

--- src/multi-chat/resources/views/components/store/modal/bot-detail.blade.php
+++ src/multi-chat/resources/views/components/store/modal/bot-detail.blade.php
@@ -113,10 +113,11 @@
                     </div>
                     <div class="w-full px-3 mt-2 flex justify-center items-center flex-wrap md:flex-nowrap">
                         <div class="w-full">
-                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white cursor-pointer"
                                 for="modelfile">Modelfile</label>
                             <div class="flex items-center">
-                                <textarea id="modelfile" name="modelfile" type="text" rows="1" max-rows="10" placeholder="modelfile"
+                                <textarea id="modelfile" name="modelfile" type="text" oninput="adjustTextareaRows(this)"
+                                    onblur="modelfile_update($(this));" rows="1" max-rows="10" placeholder="modelfile"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 resize-none"></textarea>
                             </div>
                         </div>
