1e1975de94f6b5de7d1628dad2a22e3030bf037f
黎サブ 星野 <taifu9920923@gmail.com>
2024-05-05 23:49:19 +0800

    Fix lang translation

--- src/multi-chat/lang/en_us/store.php
+++ src/multi-chat/lang/en_us/store.php
@@ -6,13 +6,13 @@ return [
     'label.offical_bots' => 'Offical Bots',
     'hint.%' => 'You must select a base model',
     'label.community_bots' => 'Community Bots',
-    'bot.system_prompt' => 'Startup Prompt',
+    'bot.system_prompt' => 'System Prompt',
     'bot.welcome_message' => 'Welcome message',
     'bot.name' => 'Name',
     'bot.name.label' => 'Bot Name',
     'bot.description' => 'Description',
     'bot.description.label' => 'Bot Description',
-    'bot.system_prompt.label' => 'The prompts will be inserted at the beginning of chatroom.',
+    'bot.system_prompt.label' => 'The prompts will change behavior of the bot',
     'bot.welcome_message.label' => 'Welcome message that shows but won\'t be passed inside when chatting.',
     'bot.button.create'=>"Create"
 ];
