0b3459de45b840264d775658c74f431da46d2634
黎サブ 星野 <taifu9920923@gmail.com>
2024-05-07 00:45:39 +0800

    Disable something that doesn't work yet

--- src/multi-chat/resources/views/components/store/modal/create-bot.blade.php
+++ src/multi-chat/resources/views/components/store/modal/create-bot.blade.php
@@ -109,7 +109,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="w-full px-3 mt-2 flex justify-center items-center flex-wrap md:flex-nowrap"
+                    <!--<div class="w-full px-3 mt-2 flex justify-center items-center flex-wrap md:flex-nowrap"
                         id="welcome_prompt">
                         <div class="w-full">
                             <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -120,7 +120,7 @@
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 resize-none"></textarea>
                             </div>
                         </div>
-                    </div>
+                    </div>-->
                     <div class="w-full px-3 mt-2 flex justify-center items-center flex-wrap md:flex-nowrap">
                         <div class="w-full">
                             <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white cursor-pointer"
