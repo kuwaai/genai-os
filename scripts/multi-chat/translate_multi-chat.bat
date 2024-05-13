53cada26959749c44f01bafc59f59a5b7f3a78ac
黎サブ 星野 <taifu9920923@gmail.com>
2024-05-13 23:51:26 +0800

    Update the batch file for translating all langs easily

---
+++ scripts/multi-chat/translate_multi-chat.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal
+set "source=../../src/multi-chat/lang"
+set "destination=translated"
+rd %destination% /S /Q
+mkdir %destination%
+for /d %%i in (%source%\*) do (
+	if not "%%~nxi" == "zh_tw" (
+		..\translate.py %source%/zh_tw.json %destination%/%%~nxi.json "This is laravel lang json file, Please translate this lang file into %%~nxi language."
+		mkdir %destination%\%%~nxi
+		for %%j in (%source%\zh_tw\*) do (
+			..\translate.py %source%/zh_tw/%%~nxj %destination%/%%~nxi/%%~nxj "This is laravel lang php file, Please translate this lang file into %%~nxi language."
+		)
+	)
+)
+endlocal
