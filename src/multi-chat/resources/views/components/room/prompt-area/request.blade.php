14a9260e2c2f60c830be2a8005be6adb17d6cf35
黎サブ 星野 <taifu9920923@gmail.com>
2024-05-13 21:42:15 +0800

    Change chain button position

--- src/multi-chat/resources/views/components/room/prompt-area/request.blade.php
+++ src/multi-chat/resources/views/components/room/prompt-area/request.blade.php
@@ -35,41 +35,40 @@
                                             ?</div>
                                     @else
                                         <img
-                                            src="{{ $llm->image ? asset(Storage::url($llm->image)) : '/'. config('app.LLM_DEFAULT_IMG') }}">
+                                            src="{{ $llm->image ? asset(Storage::url($llm->image)) : '/' . config('app.LLM_DEFAULT_IMG') }}">
                                         @endenv
                                     </div>
                                 </span>
                             @endforeach
                         </div>
                     </div>
-                    <button type="button" onclick="chain_toggle()" id="chain_btn"
-                        class="whitespace-nowrap ml-2 text-white {{ \Session::get('chained') ?? true ? 'bg-green-500 hover:bg-green-600' : 'bg-red-600 hover:bg-red-700' }} px-2 py-1 rounded">{{ \Session::get('chained') ?? true ? __('chat.button.chained') : __('chat.button.unchain') }}</button>
                 </div>
-            @else
+            @endif
+            <div class="flex w-full">
                 <button type="button" onclick="chain_toggle()" id="chain_btn"
                     class="whitespace-nowrap h-full mr-2 text-white {{ \Session::get('chained') ?? true ? 'bg-green-500 hover:bg-green-600' : 'bg-red-600 hover:bg-red-700' }} px-2 py-1 rounded">{{ \Session::get('chained') ?? true ? __('chat.button.chained') : __('chat.button.unchain') }}</button>
-            @endif
 
-            <textarea tabindex="0" data-id="root" placeholder="{{ __('chat.prompt_area.hint') }}" rows="1" max-rows="5"
-                oninput="adjustTextareaRows(this)" id="chat_input" name="input" readonly
-                class="w-full pl-4 pr-24 py-2 rounded text-black scrollbar dark:text-white placeholder-black dark:placeholder-white bg-gray-200 dark:bg-gray-600 border border-gray-300 focus:outline-none shadow-none border-none focus:ring-0 focus:border-transparent rounded-l-md resize-none"></textarea>
-            <div class="ml-auto right-[12px] relative bottom-[4px] flex justify-end items-end">
-                <label for="upload" id="upload_btn" style='display:none;'
-                    class="cursor-pointer py-1 px-3 inline-flex items-center justify-center fixed w-[32px] bg-blue-600 h-[32px] hover:bg-blue-500 dark:hover:bg-blue-700 rounded mr-10 text-white dark:text-gray-300">
-                    <i class="fas fa-paperclip"></i>
-                </label>
-                <button type="submit" id='submit_msg' style='display:none;'
-                    class="inline-flex items-center justify-center fixed w-[32px] bg-blue-600 h-[32px] rounded hover:bg-blue-500 dark:hover:bg-blue-700">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="none"
-                        class="w-5 h-5 text-white dark:text-gray-300 icon-sm m-1 md:m-0">
-                        <path
-                            d="M.5 1.163A1 1 0 0 1 1.97.28l12.868 6.837a1 1 0 0 1 0 1.766L1.969 15.72A1 1 0 0 1 .5 14.836V10.33a1 1 0 0 1 .816-.983L8.5 8 1.316 6.653A1 1 0 0 1 .5 5.67V1.163Z"
-                            fill="currentColor"></path>
-                    </svg>
-                </button>
-                <button id='abort_btn' style='display:none;' onclick="return abortGenerate()"
-                    class="text-white inline-flex items-center justify-center fixed w-[32px] bg-orange-600 h-[32px] rounded hover:bg-orange-500 dark:hover:bg-orange-700">
-                    <i class="far fa-stop-circle"></i></button>
+                <textarea tabindex="0" data-id="root" placeholder="{{ __('chat.prompt_area.hint') }}" rows="1" max-rows="5"
+                    oninput="adjustTextareaRows(this)" id="chat_input" name="input" readonly
+                    class="w-full pl-4 pr-24 py-2 rounded text-black scrollbar dark:text-white placeholder-black dark:placeholder-white bg-gray-200 dark:bg-gray-600 border border-gray-300 focus:outline-none shadow-none border-none focus:ring-0 focus:border-transparent rounded-l-md resize-none"></textarea>
+                <div class="ml-auto right-[12px] relative bottom-[4px] flex justify-end items-end">
+                    <label for="upload" id="upload_btn" style='display:none;'
+                        class="cursor-pointer py-1 px-3 inline-flex items-center justify-center fixed w-[32px] bg-blue-600 h-[32px] hover:bg-blue-500 dark:hover:bg-blue-700 rounded mr-10 text-white dark:text-gray-300">
+                        <i class="fas fa-paperclip"></i>
+                    </label>
+                    <button type="submit" id='submit_msg' style='display:none;'
+                        class="inline-flex items-center justify-center fixed w-[32px] bg-blue-600 h-[32px] rounded hover:bg-blue-500 dark:hover:bg-blue-700">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="none"
+                            class="w-5 h-5 text-white dark:text-gray-300 icon-sm m-1 md:m-0">
+                            <path
+                                d="M.5 1.163A1 1 0 0 1 1.97.28l12.868 6.837a1 1 0 0 1 0 1.766L1.969 15.72A1 1 0 0 1 .5 14.836V10.33a1 1 0 0 1 .816-.983L8.5 8 1.316 6.653A1 1 0 0 1 .5 5.67V1.163Z"
+                                fill="currentColor"></path>
+                        </svg>
+                    </button>
+                    <button id='abort_btn' style='display:none;' onclick="return abortGenerate()"
+                        class="text-white inline-flex items-center justify-center fixed w-[32px] bg-orange-600 h-[32px] rounded hover:bg-orange-500 dark:hover:bg-orange-700">
+                        <i class="far fa-stop-circle"></i></button>
+                </div>
             </div>
         </div>
     </div>
@@ -79,7 +78,6 @@
 
 <x-room.prompt-area.chat-script :llms="$llms" />
 <script>
-
     function abortGenerate() {
         $.get("{{ route('room.abort', request()->route('room_id')) }}");
         return false;
