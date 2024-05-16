fdec744f8e8d01b7020fd59894a4e174b9b6a66b
黎サブ 星野 <taifu9920923@gmail.com>
2024-05-16 20:44:19 +0800

    Use independent update.bat

--- windows/start.bat
+++ windows/start.bat
@@ -5,32 +5,6 @@ setlocal enabledelayedexpansion
 REM Include variables from separate file
 call src\variables.bat
 
-REM Check for updates
-git fetch
-git status -uno | findstr "behind"
-IF %ERRORLEVEL% EQU 0 (
-    REM Prompt the user for input with default 'Y'
-    set "response=Y"
-    set /p "response=Do you want to update? (Y/N) [Y]: "
-    
-    REM If the user just presses Enter, set response to 'Y'
-    if "!response!"=="" set response=Y
-    
-    REM Convert input to uppercase
-    set "response=!response:~0,1!"
-    
-    REM Check user response
-    if /I "!response!"=="Y" (
-        git stash
-        git pull
-        call build.bat
-    ) else (
-        echo Update skipped.
-    )
-) ELSE (
-    echo Your branch is up to date.
-)
-
 REM Redis Server
 pushd packages\%redis_folder%
 del dump.rdb
