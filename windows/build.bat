98b44b37fc7dcae882372d047108a9b2f035006d
黎サブ 星野 <taifu9920923@gmail.com>
2024-05-16 20:11:04 +0800

    Put auto update to build.bat as well

--- windows/build.bat
+++ windows/build.bat
@@ -27,6 +27,32 @@ if %ERRORLEVEL% equ 0 (
     echo Git is not installed.
 )
 
+REM Check for updates
+git fetch
+git status -uno | findstr "behind"
+IF %ERRORLEVEL% EQU 0 (
+    REM Prompt the user for input with default 'Y'
+    set "response=Y"
+    set /p "response=Do you want to update? (Y/N) [Y]: "
+    
+    REM If the user just presses Enter, set response to 'Y'
+    if "!response!"=="" set response=Y
+    
+    REM Convert input to uppercase
+    set "response=!response:~0,1!"
+    
+    REM Check user response
+    if /I "!response!"=="Y" (
+        git stash
+        git pull
+        call build.bat
+    ) else (
+        echo Update skipped.
+    )
+) ELSE (
+    echo Your branch is up to date.
+)
+
 IF EXIST packages\%python_folder% (
     echo Python folder already exists.
 ) ELSE (
