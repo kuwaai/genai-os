dc59f1d032cb1621bd2ed31337846a5e06740a31
Yung-Hsiang Hu <iftnt1999@gmail.com>
2024-06-05 01:45:18 +0800

    [windows] add: whisper executor

---
+++ windows/executors/whisper/run.bat
@@ -0,0 +1,7 @@
+set EXECUTOR_ACCESS_CODE=whisper
+set "PATH=%~dp0..\..\packages\ffmpeg-7.0.1-essentials_build\bin;%PATH%"
+echo %PATH%
+pushd ..\..\..\src\multi-chat
+php artisan model:config "whisper" "Whisper" --image "..\..\windows\executors\whisper\whisper.png"
+popd
+start /b "" "python" ..\..\..\src\executor\speech_recognition\whispers2t.py "--access_code" "whisper"
---
+++ windows/src/migration/20240604_install_whisper.bat
@@ -0,0 +1,17 @@
+REM This patch will install the dependency of the Whisper executor
+
+REM Variables for FFmpeg
+set "url_ffmpeg=https://www.gyan.dev/ffmpeg/builds/packages/ffmpeg-7.0.1-essentials_build.zip"
+for %%I in ("%url_ffmpeg%") do set "filename_ffmpeg=%%~nxI"
+set "ffmpeg_folder=%filename_ffmpeg:~0,-4%"
+for /f "tokens=2 delims=-" %%v in ("%filename_ffmpeg%") do set "version_ffmpeg=%%v"
+
+REM Download and extract FFmpeg if not exists
+call src\download_extract.bat %url_ffmpeg% packages\%ffmpeg_folder% packages\. ffmpeg.zip
+
+pushd ..\src\executor\speech_recognition
+pip install https://www.piwheels.org/simple/docopt/docopt-0.6.2-py2.py3-none-any.whl#sha256=15fde8252aa9f2804171014d50d069ffbf42c7a50b7d74bcbb82bfd5700fcfc2
+pip install -r requirements.txt
+popd
+
+exit /b 0
