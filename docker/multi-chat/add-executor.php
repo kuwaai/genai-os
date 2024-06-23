ce5a9f804ab60697e50a4aed573d29da2fd54e22
Yung-Hsiang Hu <iftnt1999@gmail.com>
2024-06-24 06:17:02 +0800

    [docker] add: model avatar support

--- docker/multi-chat/add-executor.php
+++ docker/multi-chat/add-executor.php
@@ -5,7 +5,12 @@
 
 $access_code = escapeshellarg($_POST["access_code"]);
 $name = escapeshellarg($_POST["name"]);
+$image = "";
+if (isset($_POST["image"])){
+    $image = '/app/default-icons/' . $_POST["image"];
+    $image = "--image=".escapeshellarg($image);
+}
 
-exec(sprintf("php artisan model:config %s %s 2>&1", $access_code, $name), $output, $retval);
+exec(sprintf("php artisan model:config %s %s %s 2>&1", $access_code, $name, $image), $output, $retval);
 echo "Returned with status $retval and output:\n";
 print_r($output);
