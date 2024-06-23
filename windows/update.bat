0e40327b7d89bb07a1434f283df47ee259312e77
黎サブ 星野 <taifu9920923@gmail.com>
2024-06-24 02:07:19 +0800

    Execute switch.bat at the end of update.bat

--- windows/update.bat
+++ windows/update.bat
@@ -24,6 +24,7 @@ IF %ERRORLEVEL% EQU 0 (
     if /I "!response!"=="Y" (
         git stash
         git pull
+        call src\switch.bat
         call build.bat
     ) else (
         echo Update skipped.
