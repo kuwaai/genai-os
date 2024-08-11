198c7ec082867b5640dbbdc87a1f0227179f4fac
wcwu <103374825+wcwutw@users.noreply.github.com>
2024-08-11 12:17:37 +0800

    [docker] fix: minor fix on docker installation command

--- docker/build.sh
+++ docker/build.sh
@@ -20,7 +20,7 @@ install_docker() {
 
                 # Setup repository
                 echo \
-                        "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+                        "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
                         "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
                         tee /etc/apt/sources.list.d/docker.list > /dev/null
                 apt-get update
