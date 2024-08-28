97990546824966a9d81970152aeed405a49fb8de
Hung-Jen Wang <hung-jen_wang@brown.edu>
2024-08-28 14:35:47 +0800

    openAI ollama integration completed

--- src/executor/stbot/main.py
+++ src/executor/stbot/main.py
@@ -9,21 +9,9 @@ from langchain_community.llms.ollama import Ollama
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from kuwa.executor import LLMExecutor, Modelfile
-from kuwa.client import KuwaClient
+from openai import OpenAI
 
 logger = logging.getLogger(__name__)
-
-lorem = """Hello, I am a language model nice to meet you...etc. xD
-<<<WARNING>>>
-This is a test warning
-This is the second test warning
-<<</WARNING>>>
-Warning can be outputted in the middle
-<<<WARNING>>>
-Warning 2, hi
-<<</WARNING>>>
-End of simulated text output
-"""
 
 class DummyExecutor(LLMExecutor):
     def __init__(self):
@@ -43,15 +31,6 @@ class DummyExecutor(LLMExecutor):
 
     def setup(self):
         self.stop = False
-        self.gemini = KuwaClient(
-            model='geminipro', auth_token=''
-        )
-        self.taide = KuwaClient(
-            model = 'taide-4bit', auth_token=''
-        )
-        # self.llama = KuwaClient(
-        #     model = 'llama3', auth_token=''
-        # )
 
     def get_embedding_function(self):
         embeddings = OllamaEmbeddings(model="shaw/dmeta-embedding-zh:latest")
@@ -65,33 +44,52 @@ class DummyExecutor(LLMExecutor):
         context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
         prompt_template = ChatPromptTemplate.from_template(self.PROMPT_TEMPLATE)
         prompt = prompt_template.format(context=context_text, question=query_text)
-
-        # msg = [
-        #         {"role":"user", "content": prompt}
-        # ]
-
-        # return msg
         
-        model = Ollama(model="jcai/llama3-taide-lx-8b-chat-alpha1:Q4_K_M")
-        response_text = model.invoke(prompt)
-        formatted_response = f"Response: {response_text}"
-        # print("\n"+formatted_response)
-        return response_text
+        client = OpenAI(
+            base_url = 'http://localhost:11434/v1',
+            api_key='ollama', # required, but unused
+        )
 
-    def merge(self, response_1, response_2):
+        response = client.chat.completions.create(
+            model="jcai/llama3-taide-lx-8b-chat-alpha1:Q4_K_M",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prompt}
+            ]
+        )
+        
+        return response.choices[0].message.content
+
+    async def merge(self, response_1, response_2):
         PROMPT_TEMPLATE = """
-        merge the following two parts of text into one in markdown format:
-        First part:
+        Information:
         {first}
-        Second part:
         {second}
+        ---
+        With the given information, write a one paragraph summary with markdown format in Traditional Chinese. 
+        Please remember to use Traditional Chinese.
         """
         prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
         prompt = prompt_template.format(first = response_1, second = response_2)
-        response_text = Ollama(model="llama3.1").invoke(prompt)
-        return response_text
-    
-    # Runs everytime chat is requested
+        
+        client = OpenAI(
+            base_url='http://localhost:11434/v1',
+            api_key='ollama',  # required, but unused
+        )
+        
+        response = client.chat.completions.create(
+            model = "llama3.1",
+            messages = [
+                {"role": "system", "content": "You are a healpful assistant."},
+                {"role": "user", "content": prompt}
+            ],
+            stream = True
+        )
+        
+        for chunk in response:
+            if hasattr(chunk.choices[0].delta, 'content'):
+                yield chunk.choices[0].delta.content
+        
     async def llm_compute(self, history: list[dict], modelfile:Modelfile):
         try:
             self.setup()
@@ -101,25 +99,10 @@ class DummyExecutor(LLMExecutor):
             
             original = self.query_rag(userinput, "./original")
 
-            final = self.merge(summarized, original)
-
-            yield(final)
-            
-            # async for chunk in self.taide.chat_complete(messages = summarized):
-            #     yield chunk
-            #     if self.stop:
-            #         return
-            
-            # summarized = query_rag(query, "./summary")
-            
-            # msg = [
-            #     {"role":"user", "content": f"請重複 \"{userinput}\" 三次"}
-            # ]
-            
-            # async for chunck in self.taide.chat_complete(messages = msg):
-            #     yield chunck
-            #     if self.stop:
-            #         return
+            async for chunk in self.merge(summarized, original):
+                yield chunk
+                if self.stop:
+                    return
                 
         except Exception as e:
             logger.exception("Error occurs during generation.")
