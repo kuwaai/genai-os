6b3debab793a0af265f7c8d4cae56543675d8ce9
Drift <taifu9920923@gmail.com>
2024-09-24 02:28:15 +0800

    Undo last changes on filsystems.php

--- src/multi-chat/config/filesystems.php
+++ src/multi-chat/config/filesystems.php
@@ -38,7 +38,9 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => public_path(),
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
             'throw' => false,
         ],
 
