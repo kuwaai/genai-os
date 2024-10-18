55116764fb92623fe0c051ce15044a82f7ba6894
Drift <taifu9920923@gmail.com>
2024-10-18 21:14:24 +0800

    lang update

---
+++ src/multi-chat/lang/cs_cz/cloud.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'interface.header' => 'Správa cloudových souborů',
+    'route' => 'Cloudové soubory',
+
+    'header.confirm_delete' => 'Potvrďte smazání souboru',
+    'header.cannot_preview' => 'Soubor nelze zobrazit náhledem',
+    'button.open' => 'Otevřít',
+    'button.open_tab' => 'Otevřít v nové záložce',
+    'button.copy_link' => 'Kopírovat odkaz',
+    'button.rename' => 'Přejmenovat',
+    'button.delete' => 'Smazat',
+    'button.cancel' => 'Zrušit',
+    'button.delete' => 'Smazat',
+    'button.preview' => 'Otevřít v nové záložce',
+
+    'label.homes' => 'Domovský adresář',
+    'label.no_files' => 'V adresáři nejsou žádné soubory',
+    'label.used' => 'Použito',
+    'label.total_space' => 'Celkový prostor',
+    'label.homes' => 'Domovský adresář',
+    'label.about_to_delete' => 'Chystáte se smazat soubor',
+    'label.delete_warning' => 'Tato akce je nevratná!',
+    'label.cannot_preview' => 'Omlouváme se, tento soubor nelze zobrazit náhledem. Pokračování v této akci otevře soubor v nové záložce, ale pravděpodobně se spustí automatické stahování. Pamatujte, že byste neměli spouštět neznámé spustitelné soubory nebo instalační soubory.',
+];
