35780e034bc92000a7da6cc85a8a628c70cd4811
Drift <taifu9920923@gmail.com>
2024-11-01 16:59:38 +0800

    Fix viewing tab permission in store

--- src/multi-chat/resources/views/store.blade.php
+++ src/multi-chat/resources/views/store.blade.php
@@ -80,68 +80,87 @@
                 <div class="w-full">
                     <ul class="flex w-full text-sm font-medium text-center" data-tabs-toggle="#BotContents"
                         role="tablist">
-                        <li class="flex-1" role="presentation">
-                            <button class="w-full p-4 text-gray-700 dark:text-gray-200 bg-transparent" id="system-tab"
-                                data-tabs-target="#system" type="button" role="tab" aria-controls="system"
-                                aria-selected="{{ session('last_bot_tab') ? 'false' : 'true' }}">
-                                {{ __('store.label.system_bots') }}
-                            </button>
-                        </li>
-                        <li class="flex-1" role="presentation">
-                            <button class="w-full p-4 text-gray-700 dark:text-gray-200 bg-transparent" id="private-tab"
-                                data-tabs-target="#private" type="button" role="tab" aria-controls="private"
-                                aria-selected="{{ session('last_bot_tab') == 'private' ? 'true' : 'false' }}">
-                                {{ __('store.label.private') }}
-                            </button>
-                        </li>
-                        <li class="flex-1" role="presentation">
-                            <button class="w-full p-4 text-gray-700 dark:text-gray-200 bg-transparent" id="group-tab"
-                                data-tabs-target="#group" type="button" role="tab" aria-controls="group"
-                                aria-selected="{{ session('last_bot_tab') == 'group' ? 'true' : 'false' }}">
-                                {{ __('store.label.groups_bots') }}
-                            </button>
-                        </li>
-                        <li class="flex-1" role="presentation">
-                            <button class="w-full p-4 text-gray-700 dark:text-gray-200 bg-transparent"
-                                id="community-tab" data-tabs-target="#community" type="button" role="tab"
-                                aria-controls="community"
-                                aria-selected="{{ session('last_bot_tab') == 'community' ? 'true' : 'false' }}">
-                                {{ __('store.label.community_bots') }}
-                            </button>
-                        </li>
+                        @if (request()->user()->hasPerm('Store_read_discover_system_bots') && $system_bots->count() > 0)
+                            <li class="flex-1" role="presentation">
+                                <button class="w-full p-4 text-gray-700 dark:text-gray-200 bg-transparent"
+                                    id="system-tab" data-tabs-target="#system" type="button" role="tab"
+                                    aria-controls="system"
+                                    aria-selected="{{ session('last_bot_tab') ? 'false' : 'true' }}">
+                                    {{ __('store.label.system_bots') }}
+                                </button>
+                            </li>
+                        @endif
+                        @if (request()->user()->hasPerm('Store_read_discover_private_bots') && $private_bots->count() > 0)
+                            <li class="flex-1" role="presentation">
+                                <button class="w-full p-4 text-gray-700 dark:text-gray-200 bg-transparent"
+                                    id="private-tab" data-tabs-target="#private" type="button" role="tab"
+                                    aria-controls="private"
+                                    aria-selected="{{ session('last_bot_tab') == 'private' ? 'true' : 'false' }}">
+                                    {{ __('store.label.private') }}
+                                </button>
+                            </li>
+                        @endif
+                        @if (request()->user()->hasPerm('Store_read_discover_group_bots') && $group_bots->count() > 0)
+                            <li class="flex-1" role="presentation">
+                                <button class="w-full p-4 text-gray-700 dark:text-gray-200 bg-transparent"
+                                    id="group-tab" data-tabs-target="#group" type="button" role="tab"
+                                    aria-controls="group"
+                                    aria-selected="{{ session('last_bot_tab') == 'group' ? 'true' : 'false' }}">
+                                    {{ __('store.label.groups_bots') }}
+                                </button>
+                            </li>
+                        @endif
+                        @if (request()->user()->hasPerm('Store_read_discover_community_bots') && $community_bots->count() > 0)
+                            <li class="flex-1" role="presentation">
+                                <button class="w-full p-4 text-gray-700 dark:text-gray-200 bg-transparent"
+                                    id="community-tab" data-tabs-target="#community" type="button" role="tab"
+                                    aria-controls="community"
+                                    aria-selected="{{ session('last_bot_tab') == 'community' ? 'true' : 'false' }}">
+                                    {{ __('store.label.community_bots') }}
+                                </button>
+                            </li>
+                        @endif
                     </ul>
                 </div>
 
                 <div id="BotContents" class="flex flex-1 overflow-hidden px-4 mb-2">
                     <!-- System Bots Tab Content -->
-                    @if (request()->user()->hasPerm('Store_read_discover_system_bots') && $system_bots->count() > 0)
-                        <div class="{{ session('last_bot_tab') ? 'hidden' : '' }} flex flex-1"
-                            id="system" role="tabpanel" aria-labelledby="system-tab">
+                    @if (request()->user()->hasPerm('Store_read_discover_system_bots'))
+                        <div class="{{ session('last_bot_tab') ? 'hidden' : '' }} flex flex-1" id="system"
+                            role="tabpanel" aria-labelledby="system-tab">
+                            @if ($system_bots->count() > 0)
                             <x-store.bot-showcase :bots="$system_bots" :extra="'official_bots-'" />
+                            @endif
                         </div>
                     @endif
 
                     <!-- Private Bots Tab Content -->
-                    @if (request()->user()->hasPerm('Store_read_discover_private_bots') && $private_bots->count() > 0)
+                    @if (request()->user()->hasPerm('Store_read_discover_private_bots'))
                         <div class="{{ session('last_bot_tab') == 'private' ? '' : 'hidden' }} flex flex-1"
                             id="private" role="tabpanel" aria-labelledby="private-tab">
+                            @if($private_bots->count() > 0)
                             <x-store.bot-showcase :bots="$private_bots" :extra="'my_bots-'" />
+                            @endif
                         </div>
                     @endif
 
                     <!-- Group Bots Tab Content -->
-                    @if (request()->user()->hasPerm('Store_read_discover_group_bots') && $group_bots->count() > 0)
+                    @if (request()->user()->hasPerm('Store_read_discover_group_bots'))
                         <div class="{{ session('last_bot_tab') == 'group' ? '' : 'hidden' }} flex flex-1"
                             id="group" role="tabpanel" aria-labelledby="group-tab">
+                            @if($group_bots->count() > 0)
                             <x-store.bot-showcase :bots="$group_bots" :extra="'group_bots-'" />
+                            @endif
                         </div>
                     @endif
 
                     <!-- Community Bots Tab Content -->
-                    @if (request()->user()->hasPerm('Store_read_discover_community_bots') && $community_bots->count() > 0)
+                    @if (request()->user()->hasPerm('Store_read_discover_community_bots'))
                         <div class="{{ session('last_bot_tab') == 'community' ? '' : 'hidden' }} flex flex-1"
                             id="community" role="tabpanel" aria-labelledby="community-tab">
+                            @if($community_bots->count() > 0)
                             <x-store.bot-showcase :bots="$community_bots" :extra="'community_bots-'" />
+                            @endif
                         </div>
                     @endif
                 </div>
