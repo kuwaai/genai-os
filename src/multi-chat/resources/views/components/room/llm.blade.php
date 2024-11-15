adfb997bccc0087d3f95e2a30af0563745a81540
Drift <taifu9920923@gmail.com>
2024-11-15 13:29:03 +0800

    Make button shorter

--- src/multi-chat/resources/views/components/room/llm.blade.php
+++ src/multi-chat/resources/views/components/room/llm.blade.php
@@ -10,7 +10,7 @@
                     <form method="post" action="{{ route('room.new') }}">
                         @csrf
                         <button
-                            class="flex items-center px-2 scrollbar rounded-t-lg w-full hover:bg-gray-300 dark:hover:bg-gray-700 scrollbar-3 overflow-x-auto py-3 border-b border-black dark:border-white">
+                            class="flex items-center px-2 scrollbar rounded-t-lg w-full hover:bg-gray-300 dark:hover:bg-gray-700 scrollbar-3 overflow-x-auto py-2 border-b border-black dark:border-white">
                             @include('components.room.chat_button', ['chats' => $chats, 'extra' => $extra])
                             @if (count($chats) == 1)
                                 <span
@@ -20,7 +20,7 @@
                     </form>
                 @else
                     <div
-                        class="flex px-2 scrollbar rounded-t-lg w-full scrollbar-3 overflow-x-auto py-3 border-b border-black dark:border-white">
+                        class="flex px-2 scrollbar rounded-t-lg w-full scrollbar-3 overflow-x-auto py-2 border-b border-black dark:border-white">
                         @include('components.room.chat_button', ['chats' => $chats, 'extra' => $extra])
                         @if (count($chats) == 1)
                             <span
@@ -46,7 +46,8 @@
                                     <path fill-rule="evenodd" clip-rule="evenodd"
                                         d="M3 12C3 10.8954 3.89543 10 5 10C6.10457 10 7 10.8954 7 12C7 13.1046 6.10457 14 5 14C3.89543 14 3 13.1046 3 12ZM10 12C10 10.8954 10.8954 10 12 10C13.1046 10 14 10.8954 14 12C14 13.1046 13.1046 14 12 14C10.8954 14 10 13.1046 10 12ZM17 12C17 10.8954 17.8954 10 19 10C20.1046 10 21 10.8954 21 12C21 13.1046 20.1046 14 19 14C17.8954 14 17 13.1046 17 12Z"
                                         fill="currentColor"></path>
-                                </svg></button>
+                                </svg>
+                            </button>
                             <div id="{{ $extra }}chat_dropdown_{{ $dc->id }}"
                                 class="z-10 hidden bg-gray-200 border border-1 dark:border-white border-black divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
                                 <ul class="py-2 text-sm text-gray-700 dark:text-gray-200"
