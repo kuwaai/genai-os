a36f0705989bea92451f36103ddbd70ac3f7b983
Drift <taifu9920923@gmail.com>
2024-11-18 09:57:23 +0800

    Use Laravel scheduler

--- src/multi-chat/app/Console/Kernel.php
+++ src/multi-chat/app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Jobs\CheckUpdate;
 
 class Kernel extends ConsoleKernel
 {
@@ -12,7 +13,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->job(new CheckUpdate(true))->everyFiveMinutes();
     }
 
     /**
