a63e2918ae0052699eeb047f94a40597e3c6eb48
Drift <taifu9920923@gmail.com>
2024-11-19 19:31:42 +0800

    Remove tooltip and show bots in action popup

--- src/multi-chat/resources/views/components/room/llm.blade.php
+++ src/multi-chat/resources/views/components/room/llm.blade.php
@@ -11,19 +11,7 @@
             <h3 class="my-2 font-bold text-xs text-gray-800 dark:text-white text-center">{{ $group }}</h3>
 
             @foreach ($chatRooms as $dc)
-                <div id="chatroom_{{$extra}}_{{ $dc->id }}" role="tooltip"
-                    class="absolute flex z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-500">
-
-                    @foreach (App\Models\Bots::getBotsFromIds(explode(',', $dc->identifier)) as $llm)
-                        <div
-                            class="mx-1 flex-shrink-0 h-5 w-5 rounded-full bg-black flex items-center justify-center overflow-hidden">
-                            <img class="h-full w-full"
-                                src="{{ $llm->image ? asset(Storage::url($llm->image)) : '/' . config('app.LLM_DEFAULT_IMG') }}">
-                        </div>
-                    @endforeach
-                    <div class="tooltip-arrow" data-popper-arrow></div>
-                </div>
-                <div class="rounded-lg" data-tooltip-target="chatroom_{{$extra}}_{{ $dc->id }}" data-tooltip-placement="top">
+                <div class="rounded-lg">
                     <div class="max-h-[182px] overflow-y-auto scrollbar">
                         <div class="overflow-hidden rounded mb-1 flex dark:hover:bg-gray-700 hover:bg-gray-200">
                             <a class="menu-btn flex-1 text-gray-700 px-2 dark:text-white w-full flex justify-start items-center overflow-hidden {{ request()->route('room_id') == $dc->id ? 'bg-gray-200 dark:bg-gray-700' : '' }} transition duration-300"
@@ -46,6 +34,15 @@
                                 class="z-10 hidden bg-gray-200 border border-1 dark:border-white border-black divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
                                 <ul class="py-2 text-sm text-gray-700 dark:text-gray-200"
                                     aria-labelledby="dropdownDefaultButton">
+                                    <li class='flex px-4 py-2'>
+                                        @foreach (App\Models\Bots::getBotsFromIds(explode(',', $dc->identifier)) as $llm)
+                                            <div
+                                                class="mx-1 flex-shrink-0 h-5 w-5 rounded-full bg-black flex items-center justify-center overflow-hidden">
+                                                <img class="h-full w-full"
+                                                    src="{{ $llm->image ? asset(Storage::url($llm->image)) : '/' . config('app.LLM_DEFAULT_IMG') }}">
+                                            </div>
+                                        @endforeach
+                                    </li>
                                     <li>
                                         <a href="{{ route('room.share', $dc->id) }}" target="_blank"
                                             class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white !text-green-500 hover:!text-green-600">{{ __('room.button.share_link') }}</a>
